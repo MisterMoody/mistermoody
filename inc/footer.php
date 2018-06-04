4b132b01248905d9cb13a216ea06242295addb4c
Mister <principal@mistermoody.com>
2018-06-04 16:59:37 -0400

    Update Site with PHP Template Layout

---
+++ inc/footer.php
@@ -0,0 +1,28 @@
+
+
+
+
+
+        
+      <!-- ........END// -->
+      <!-- //FOOTER --> 
+      <footer class="credit">
+        <small>&#169;<span class=""><?php echo date('Y'); ?></span></small>&#32;&#32;
+        <span class=""> Made with <i class="fa fa-heart"></i> by </span> Mister Moody
+      </footer>  
+    </div>
+    <!-- ************************* -->
+    
+    <!-- JAVASCRIPT ************** --> 
+    <!-- \\\\\\ Font-Awesome Icons -->
+    <script src="https://use.fontawesome.com/ceeedfe13b.js"></script>
+    <!-- \\\\\\\\\\\ Reportcard JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <script src="js/reportcard/js/reportcard.js"></script>
+    <script>$('.report-card.treehouse').reportCard({
+            userName: 'mistermoody',
+            site: 'treehouse',
+            badgesAmount: 7});</script>
+    <!-- ************************* --> 
+  </body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mister Moody</title>
+    <meta name='description' content='Mister Moody: Virtual Assistant Based in Louisville Kentucky' />
+    <meta name="keywords" content="Edupreneur, Teacher, Tutor, Virtual Assistant, Web Designer, TEFL, ESL" />
+    <!-- Browser Tab Icon -->
+    <link rel="shortcut icon" href="img/me.jpg" type="img/m" class="rounded">    
+    <!-- Custom CSS Styles -->   
+    <link rel="stylesheet" href="css/grid.css">
+    
+    <link rel="stylesheet" href="css/main.css">
+    <!-- Custom Fonts -->       
+	  <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Raleway" rel="stylesheet">
+    <!-- FontAwesome CSS Fonts -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Treehouse ReportCard by Riley Hilliard -->
+    <link href="js/reportcard/css/badges.css" rel="stylesheet">
+  </head>
+  <body>
+  <!-- ********************************************************* -->
+  <!-- ********************************************************* -->
+    <!-- ****** <MAIN-GRID>  ***** -->
+    <div class="grid">    
+      <!-- //HEADER Navigation -->
+      <header>
+        <ul class="nav">
+          <li class="home">
+            <a href="index.php" alt="Homepage">Mister Moody
+            <!--<i class="fa fa-home fa-2x" aria-hidden="true"></i>--></a>
+          </li>
+          <li class="about">
+            <a href="about.php" alt="About Page" title="About Mister Moody">About
+            <!--<i class="fa fa-user fa-2x" aria-hidden="true"></i>--></a>
+          </li>
+          <li class="portfolio">
+            <a href="portfolio.php" alt="Portfolio" title="Check Out My Work">Portfolio
+            <!--<i class="fa fa-briefcase fa-2x" aria-hidden="true"></i>--></a>
+          </li>
+          <li class="contact">
+            <a href="contact.php" alt="Get in Touch!" title="Get in Touch!">Contact
+            <!--<i class="fa fa-envelope fa-2x" aria-hidden="true"></i>--></a>
+          </li>
+        </ul>
+      </header>
+      <!-- //CONTENT -->
+      <main>
+        <!-- //BEGIN...... -->
