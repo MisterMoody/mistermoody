6386c77c84eef52cdcf39ec0fdb010bca6583934
Mister <principal@mistermoody.com>
2018-06-05 19:32:30 -0400

    Update Portfolio Image Gallery

--- portfolio.php
+++ portfolio.php
@@ -8,28 +8,24 @@ include ("inc/header.php");
 <!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 
 <h2>It ain't ready...</h2>
-<p>...until it is, look at some snaps from my travels abroad in the gallery below or read some code I wrote for my current projects <a href="https://github.com/MisterMoody" alt="Github" target="_blank" title="Check Out My Work on Github">here</a>.</p>
+<p>...until it is, look at some snaps from my travels abroad in the gallery below or read some code I wrote for my current projects <a href="https://github.com/MisterMoody" alt="Github" target="_blank" title="Check Out My Work on Github">here</a>.</p><!-- -->
 <section class="gallery">
-    <div class="item"><img src="img/7.JPG"/></div>
+<!-- -->    <div class="item"><img src="img/7.JPG"/></div>
+<!-- -->    <div class="item"><img src="img/cnvisa.jpg"/></div>
     <div class="item"><img src="img/yungn.JPG"/></div>
     <div class="item"><img src="img/jpnyahata.jpg" /></div>
-    <div class="item"><img src="img/workVanilla.jpg"/></div>
+<!-- -->    <div class="item"><img src="img/hk.JPG"/></div>
     <div class="item"><img src="img/jpnID.jpg"/></div>
-    <div class="item"><img src="img/cnvisa.jpg"/></div>
     <div class="item"><img src="img/amsterdam.jpg"/></div>
-    <div class="item"><img src="img/hk.JPG"/></div>
-    <div class="item"><img src="img/free.jpg"/></div>
-    <div class="item"><img src="img/jpnvisa.jpg"/></div>
-    <div class="item"><img src="img/eat.jpg"/></div>
-    <div class="item"><img src="img/jpnvisa.jpg"/></div>
+    <div class="item"><img src="img/workVanilla.jpg"/></div>
     <div class="item"><img src="img/6.JPG"/></div>
-    <div class="item"><img src="img/jpnstamp.jpg"/></div>
-    <div class="item"><img src="img/jdhs.jpg"/></div>
-    <div class="item"><img src="img/jpnyahata.jpg"/></div>
-    <div class="item"><img src="img/narita08.JPG"/></div>
+<!-- -->    <div class="item"><img src="img/jpnvisa.jpg"/></div>
+    <div class="item"><img src="img/eat.jpg"/></div>
     <div class="item"><img src="img/cn09.JPG"/></div>
+    <div class="item"><img src="img/jpnstamp.jpg"/></div>
+    <div class="item"><img src="img/narita08.JPG"/></div>
+    <div class="item"><img src="img/jdhs.jpg"/></div>
     <div class="item"><img src="img/conscious.jpg"/></div>
-    <div class="item"><img src="img/tongxiang.jpg"/></div>
 </section>
 <!-- -->
 <!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
