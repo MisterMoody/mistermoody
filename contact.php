387c67b06f8a04deec412631ef5e61e67c149fe4
Mister <principal@mistermoody.com>
2018-06-08 11:20:54 -0400

    Modify Contact Form Layout

--- contact.php
+++ contact.php
@@ -31,19 +31,19 @@ include ("inc/header.php");
               <!-- Name -->
               <div class="formName">
                 <label for="name" class=""></label>
-                <input type="text" id="name" name="sender" placeholder=" Name" title="Please provide your name." size="44%" pattern="^([- \w\d\u00c0-\u024f]+)$" required aria-required="true" aria-describedby="prospective" class="form-control" >
+                <input type="text" id="name" name="sender" placeholder=" Name" title="Please provide your name." size="44px" pattern="^([- \w\d\u00c0-\u024f]+)$" required aria-required="true" aria-describedby="prospective" class="form-control" >
                 <br>
               </div>
               <!-- Email -->
               <div class="formEmail">
                 <label for="email" class="" id="prospectiveEmail"></label>
-                <input type="email" id="email"  placeholder=" Email Address" value="" name="senderEmail" size="44%" pattern="^(([-\w\d]+)(\.[-\w\d]+)*@([-\w\d]+)(\.[-\w\d]+)*(\.([a-zA-Z]{2,5}|[\d]{1,3})){1,2})$" required aria-required="true" aria-describedby="prospectiveEmail" class="" title="Please provide your email.">
+                <input type="email" id="email"  placeholder=" Email Address" value="" name="senderEmail" size="44px" pattern="^(([-\w\d]+)(\.[-\w\d]+)*@([-\w\d]+)(\.[-\w\d]+)*(\.([a-zA-Z]{2,5}|[\d]{1,3})){1,2})$" required aria-required="true" aria-describedby="prospectiveEmail" class="" title="Please provide your email.">
                 <br>
               </div>
               <!-- Message -->
               <div class="formMSG">
                 <label for="message" class="" id="prospectiveMSG"></label>
-                <textarea type="text" id="message" name="message" placeholder=" Write your Message Here" class="" size="44%" rows="4" maxlength="5000" required aria-required="true" aria-describedby="prospectiveMSG" title="Please write a detailed message describing your project, scope, timetable and any immediate concerns that you have."></textarea>
+                <textarea type="text" id="message" name="message" placeholder=" Write your Message Here" class="" size="44px" rows="4" maxlength="5000" required aria-required="true" aria-describedby="prospectiveMSG" title="Please write a detailed message describing your project, scope, timetable and any immediate concerns that you have."></textarea>
               </div>
               <!-- Submit >> Modal Trigger -->
               <div class="formSubmit">
