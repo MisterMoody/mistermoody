d35e55ab3169211244e4e43bdce1ade64c19ff86
MisterMoody <principal@mistermoody.com>
2018-10-06 16:06:34 -0400

    Remove Files

--- inc/vcardFoot.php
+++
@@ -1,7 +0,0 @@
-    <!-- ......//END -->
-    <!-- JAVASCRIPT ************** --> 
-    <!-- \\\\\\ Font-Awesome Icons -->
-    <script src="https://use.fontawesome.com/ceeedfe13b.js"></script>
-    <!-- ************************* --> 
-  </body>
-</html>
