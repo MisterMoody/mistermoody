28cab2d7d6892f928d0cf0c05723a26e815793e1
MisterMoody <principal@mistermoody.com>
2019-01-03 22:47:58 -0500

    Update Portfolio Gallery Grid

--- portfolio.php
+++ portfolio.php
@@ -4,8 +4,8 @@ $pageTitle = "Portfolio Gallery for Mister Moody";
 
 include ("inc/header.php");
 ?>
-<!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
-<!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+<!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+<!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 
 <section>
   <div>
@@ -15,25 +15,24 @@ include ("inc/header.php");
 <section class="gallery">
   <div class="item"><img src="img/cnID.jpg"/></div>
   <div class="item"><img src="img/cnvisa.jpg"/></div>
-  <div class="item"><img src="img/yungn.JPG"/></div>
-  <div class="item"><img src="img/hk.JPG"/></div>
-  <div class="item"><img src="img/cn09.JPG"/></div>
-  <div class="item"><img src="img/6.JPG" /></div>
-  <div class="item"><img src="img/conscious.jpg"/></div>
   <div class="item"><img src="img/jdhs.jpg"/></div>
   <div class="item"><img src="img/jpnyahata.jpg"/></div>
-  <div class="item"><img src="img/eat.jpg"/></div>
-  <div class="item"><img src="img/jpnvisa.jpg"/></div>
-  <div class="item"><img src="img/jpnID.jpg"/></div>
-  <div class="item"><img src="img/jpnstamp.jpg"/></div>
+  <div class="item"><img src="img/MMgrey.jpg"/></div>
+  <div class="item"><img src="img/hk.JPG"/></div>
   <div class="item"><img src="img/narita08.JPG"/></div>
-  <div class="item"><img src="img/workVanilla.jpg"/></div>
+  <div class="item"><img src="img/jpnstamp.jpg"/></div>
+  <div class="item"><img src="img/jpnvisa.jpg"/></div>
+  <div class="item"><img src="img/yungn.JPG"/></div>
   <div class="item"><img src="img/amsterdam.jpg"/></div>
+  <div class="item"><img src="img/6.JPG" /></div>
+  <div class="item"><img src="img/jpnID.jpg"/></div>
+  <div class="item"><img src="img/conscious.jpg"/></div>
+  <div class="item"><img src="img/eat.jpg"/></div>
+  <div class="item"><img src="img/workVanilla.jpg"/></div>
   <div class="item"><img src="img/free.jpg"/></div>
 </section>
-<!-- -->
-<!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
-<!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->       
+<!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+<!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->       
 <?php
 include ("inc/footer.php"); 
 ?>
