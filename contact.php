63a44004efdc60556e8cd5e400d5207d3f475911
MisterMoody <principal@mistermoody.com>
2019-01-15 11:37:25 -0500

    Contact Form Layout Mod

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@ include ("inc/header.php");
 
 <!-- //CONTENT-BEGINS..... -->
 <main>
-  <section class="contactForm">
+  <section>
     <h3>Share Your Thoughts!</h3>
     <!-- PHP -->
     <?php
@@ -27,25 +27,25 @@ include ("inc/header.php");
           }
     ?>
     <!-- 'FORM'-->
-    <form method="post" action="contact.php" name="contactForm">
-      <div class="form" id="contact_form" href="thankyou.html">
+    <form class="contactForm" method="post" action="contact.php" name="contactForm">
+      <div href="thankyou.html">
         <!-- Name -->
-        <div class="formName">
+        <div>
           <label for="name"></label>
           <input type="text" id="name" name="sender" placeholder=" Name" title="Please provide your name." size="33vw" pattern="^([- \w\d\u00c0-\u024f]+)$" required aria-required="true" aria-describedby="prospective" class="form-control">
         </div>
         <!-- Email -->
-        <div class="formEmail">
+        <div>
           <label for="email" id="prospectiveEmail"></label>
           <input type="email" id="email"  placeholder=" Email Address" value="" name="senderEmail" size="33vw" pattern="^(([-\w\d]+)(\.[-\w\d]+)*@([-\w\d]+)(\.[-\w\d]+)*(\.([a-zA-Z]{2,5}|[\d]{1,3})){1,2})$" required aria-required="true" aria-describedby="prospectiveEmail" title="Please provide your email.">
         </div>
         <!-- Message -->
-        <div class="formMSG">
+        <div>
           <label for="message" id="prospectiveMSG"></label>
           <textarea type="text" id="message" name="message" placeholder="Share your thoughts..." cols="35" rows="7" maxlength="5000" required aria-required="true" aria-describedby="prospectiveMSG" title="Please write a detailed message describing your project, scope, timetable and any immediate concerns that you have."></textarea>
         </div>
         <!-- Submit >> Modal Trigger -->
-        <div class="formSubmit">
+        <div>
           <input type="submit" name="submit" value="Submit" id="submit_button">
         </div>
         <!---->
