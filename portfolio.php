f92bc376316faec81552b8b175524637d5f7a5ed
MisterMoody <principal@mistermoody.com>
2019-01-30 21:37:10 -0500

    Create <code> Glitch for Page Title

--- portfolio.php
+++ portfolio.php
@@ -5,36 +5,68 @@ include ("inc/header.php");
 <!-- ******************************************************* -->
 <!-- *************** <<PORTFOLIO GALLERY>>  **************** -->
 
-
+<main>
 <!-- //CONTENT-BEGINS..... -->
 <section>
-  <div>
-    <h3>It ain't ready...</h3>
-    <p class="portText">...until it is, look at some snaps from my travels abroad in the gallery below or read over a resources catalog that I am curating on all things related to <u><a href="https://mistermoody.github.io/" alt="Link to Portfolio" target="_blank" title="Check Out My Work on Github">Web Design</a></u>.</p>
+  <div class="cat__title">
+    <code class="cat__code">
+      .malfunction {
+      <br>&nbsp;&nbsp;in-progress: nearly-finished;
+      <br>}</code>
   </div>
 </section>
 <!-- -->
-<section class="gallery">
-  <div class="item"><img src="img/cnID.jpg"/></div>
-  <div class="item"><img src="img/cnvisa.jpg"/></div>
-  <div class="item"><img src="img/jdhs.jpg"/></div>
-  <div class="item"><img src="img/jpnyahata.jpg"/></div>
-  <div class="item"><img src="img/MMgrey.jpg"/></div>
-  <div class="item"><img src="img/hk.JPG"/></div>
-  <div class="item"><img src="img/narita08.JPG"/></div>
-  <div class="item"><img src="img/jpnstamp.jpg"/></div>
-  <div class="item"><img src="img/jpnvisa.jpg"/></div>
-  <div class="item"><img src="img/yungn.JPG"/></div>
-  <div class="item"><img src="img/amsterdam.jpg"/></div>
-  <div class="item"><img src="img/6.JPG" /></div>
-  <div class="item"><img src="img/jpnID.jpg"/></div>
-  <div class="item"><img src="img/conscious.jpg"/></div>
-  <div class="item"><img src="img/eat.jpg"/></div>
-  <div class="item"><img src="img/workVanilla.jpg"/></div>
-  <div class="item"><img src="img/free.jpg"/></div>
+<section class="category">
+  <div class="cat__container"><!-- LEFT -->
+    <img src="https://via.placeholder.com/350x255/FF0000/FFFFFF.png?text=Web Design" 
+					 alt="img" class="cat__image">
+    <div class="overlay overlayLeft">
+      <div class="cat__text">Learn the basics of <a href="https://mistermoody.github.io/" alt="Link to Portfolio" target="_blank" title="Check Out My Work on Github">Web Design</a> using HTML and CSS</div>
+    </div>
+  </div>
+
+	<div class="cat__container"><!-- RIGHT -->
+		<img src="https://via.placeholder.com/350x255/FF0000/FFFFFF.png?text=ESL / TEFL" 
+					 alt="img" class="cat__image">
+		<div class="overlay overlayRight">
+			<div class="cat__text">Learn English from a Native Speaker</div>
+		</div>
+	</div>
+
+	<div class="cat__container"><!-- TOP -->
+		<img src="https://via.placeholder.com/350x255/FF0000/FFFFFF.png?text=Mandarin" 
+					 alt="img" class="cat__image">
+		<div class="overlay overlayTop">
+			<div class="cat__text">Learn Mandarin from an Experienced Traveler</div>
+		</div>
+	</div>
+
+	<div class="cat__container"><!-- BOTTOM -->
+		<img src="https://via.placeholder.com/350x255/FF0000/FFFFFF.png?text=Aborigine" 
+					 alt="img" class="cat__image">
+		<div class="overlay overlayBottom">
+			<div class="cat__text">a Self-Discovery project to better understand my ancestry</div>
+		</div>
+	</div>
+
+	<div class="cat__container"><!-- CROSS -->
+		<img src="https://via.placeholder.com/350x255/FF0000/FFFFFF.png?text=Health" 
+					 alt="img" class="cat__image">
+		<div class="overlay cat__overlayCrossL">
+			<div class="cat__text">Getting Fit is a Cinch</div>
+		</div>
+	</div>	
+
+	<div class="cat__container"><!-- FADE -->
+		<img src="https://via.placeholder.com/350x255/FF0000/FFFFFF.png?text=Mister Moody" 
+					 alt="img" class="cat__image">
+		<div class="overlay cat__overlayCrossR">
+			<div class="cat__text">Fade</div>
+		</div>
+	</div>
 </section>
 <!-- .....CONTENT-ENDS// --> 
-
+</main>
 
 <!-- ******************************************************* -->
 <!-- ******************************************************* -->
