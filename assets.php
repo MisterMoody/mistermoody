4410e3d184ccea12ca44136d3d32074b30e9aeba
MisterMoody <principal@mistermoody.com>
2019-01-30 19:09:00 -0500

    Create HTML/CSS 'Assets' Page

---
+++ assets.php
@@ -0,0 +1,53 @@
+<?php
+$pageTitle = "Mister Moody ASSETS";
+include ("inc/header.php");
+?>
+<!-- ******************************************************* -->
+<!-- *************** <<PORTFOLIO GALLERY>>  **************** -->
+
+
+<!-- //CONTENT-BEGINS..... -->
+<main class="assets">
+<section>
+  <div>
+    <h1>ASSETS</h1>
+    <p>A collection of assets.</p>
+  </div>
+</section>
+<!-- -->
+<section class="gallery">
+  <div class="item"><img src="img/cnID.jpg"/></div>
+  <div class="item"><img src="img/cnvisa.jpg"/></div>
+  <div class="item"><img src="img/jdhs.jpg"/></div>
+  <div class="item"><img src="img/jpnyahata.jpg"/></div>
+  <div class="item"><img src="img/MMgrey.jpg"/></div>
+  <div class="item"><img src="img/hk.JPG"/></div>
+  <div class="item"><img src="img/narita08.JPG"/></div>
+  <div class="item"><img src="img/jpnstamp.jpg"/></div>
+  <div class="item"><img src="img/jpnvisa.jpg"/></div>
+  <div class="item"><img src="img/yungn.JPG"/></div>
+  <div class="item"><img src="img/amsterdam.jpg"/></div>
+  <div class="item"><img src="img/6.JPG" /></div>
+  <div class="item"><img src="img/jpnID.jpg"/></div>
+  <div class="item"><img src="img/conscious.jpg"/></div>
+  <div class="item"><img src="img/eat.jpg"/></div>
+  <div class="item"><img src="img/workVanilla.jpg"/></div>
+  <div class="item"><img src="img/free.jpg"/></div>
+</section>
+  <!-- -->
+  <section>
+    <aside class="newsLinkGrid">
+      <div class="newsLinkImage">image</div>
+      <div class="newsTitle">title</div>
+      <div class="newsLinkStory">story</div>
+    </aside>
+  </section>
+</main>
+<!-- .....CONTENT-ENDS// --> 
+
+
+<!-- ******************************************************* -->
+<!-- ******************************************************* -->
+<?php
+include ("inc/footer.php"); 
+?>
