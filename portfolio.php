3c96f5100af8112e60fd4c02d7dee4047d256f6f
MisterMoody <principal@mistermoody.com>
2019-01-30 22:12:49 -0500

    Add 'Category-Grid' Layout for Portfolio.php + Styles

--- portfolio.php
+++ portfolio.php
@@ -18,7 +18,7 @@ include ("inc/header.php");
 <!-- -->
 <section class="category">
   <div class="cat__container"><!-- LEFT -->
-    <img src="https://via.placeholder.com/350x255/FF0000/FFFFFF.png?text=Web Design" 
+    <img src="https://via.placeholder.com/350x255/000000/FFFFFF.png?text=Web Design" 
 					 alt="img" class="cat__image">
     <div class="overlay overlayLeft">
       <div class="cat__text">Learn the basics of <a href="https://mistermoody.github.io/" alt="Link to Portfolio" target="_blank" title="Check Out My Work on Github">Web Design</a> using HTML and CSS</div>
@@ -26,42 +26,27 @@ include ("inc/header.php");
   </div>
 
 	<div class="cat__container"><!-- RIGHT -->
-		<img src="https://via.placeholder.com/350x255/FF0000/FFFFFF.png?text=ESL / TEFL" 
+		<img src="https://via.placeholder.com/350x255/000000/FFFFFF.png?text=Lesson Plans" 
 					 alt="img" class="cat__image">
-		<div class="overlay overlayRight">
-			<div class="cat__text">Learn English from a Native Speaker</div>
+    <div class="cat__image"></div>
+		<div class="overlay overlayTop">
+			<div class="cat__text">Bespoke curriculum materials for learners </div>
 		</div>
 	</div>
 
 	<div class="cat__container"><!-- TOP -->
-		<img src="https://via.placeholder.com/350x255/FF0000/FFFFFF.png?text=Mandarin" 
+		<img src="https://via.placeholder.com/350x255/000000/FFFFFF.png?text=Mandarin" 
 					 alt="img" class="cat__image">
-		<div class="overlay overlayTop">
+		<div class="overlay overlayBottom">
 			<div class="cat__text">Learn Mandarin from an Experienced Traveler</div>
 		</div>
 	</div>
 
 	<div class="cat__container"><!-- BOTTOM -->
-		<img src="https://via.placeholder.com/350x255/FF0000/FFFFFF.png?text=Aborigine" 
+		<img src="https://via.placeholder.com/350x255/000000/FFFFFF.png?text=Aborigine" 
 					 alt="img" class="cat__image">
-		<div class="overlay overlayBottom">
+		<div class="overlay overlayRight">
 			<div class="cat__text">a Self-Discovery project to better understand my ancestry</div>
-		</div>
-	</div>
-
-	<div class="cat__container"><!-- CROSS -->
-		<img src="https://via.placeholder.com/350x255/FF0000/FFFFFF.png?text=Health" 
-					 alt="img" class="cat__image">
-		<div class="overlay cat__overlayCrossL">
-			<div class="cat__text">Getting Fit is a Cinch</div>
-		</div>
-	</div>	
-
-	<div class="cat__container"><!-- FADE -->
-		<img src="https://via.placeholder.com/350x255/FF0000/FFFFFF.png?text=Mister Moody" 
-					 alt="img" class="cat__image">
-		<div class="overlay cat__overlayCrossR">
-			<div class="cat__text">Fade</div>
 		</div>
 	</div>
 </section>
