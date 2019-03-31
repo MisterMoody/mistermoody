acb09778179d92c1924f6aa96a782ba6fb37a374
MisterMoody <principal@mistermoody.com>
2019-03-31 10:58:42 -0400

    Site Structure Overhaul

--- inc/footer.php
+++ inc/footer.php
@@ -1,36 +1,41 @@
-      <!-- //FOOTER --> 
-      <footer class="credit">
-        <small>&#169;<span>2015 - <?php echo date('Y'); ?></span>&nbsp;&nbsp; <b>Ray Moody dba Mister Moody</b>&nbsp;&nbsp; <br>All Rights Reserved.</small>
-      </footer>
-      <!---->
-      <!-- //CONTENT-ENDS..... -->
-      <!----> 
-    </article>
 
-    <!-- ^^SCROLL-to-TOP BUTTON^^ -->
+
+      <!-- ***************************** -->
+      <!-- // CONTENT-ENDS.............. -->
+      <!-- ***************************** -->   
+
+      <!-- // FOOTER ******************* --> 
+      <footer>
+        <small>&#169;<span><?php echo date('Y'); ?></span></small>&#32;&#32;
+        Ray Moody dba Mister Moody
+      </footer>
+      <!-- ***************************** -->   
+
+    </article>
+  <!-- ********************************* -->
+  <!-- ** // GRID CONTAINER 'end' ****** -->
+  <!-- ********************************* -->
+
+    <!-- // SCROLL-to-TOP BUTTON ******* -->
     <button class="" onclick="topFunction()" id="myBtn" title="Go to top">
-  <a class="viewDesktop" href="" alt="Back to Top" title="Back to Top">
-    <i class="fa fa-arrow-circle-up fa-2x" aria-hidden="true"></i>
-  </a>
-</button>
-    <!-- //'end of' MAIN GRID CONTAINER-->	
-    <!-- ********************************************************* -->
-    <!-- ********************************************************* -->
-    
-    <!-- JAVASCRIPT ************** --> 
-    <!-- \\\\\\ SIDE-Bar Navigation         -->
-    <script src="inc/siteNav.js"></script>
-    <!-- \\\\\\ Scroll-to-Top      -->
+      <a class="viewDesktop" href="" alt="Back to Top" title="Back to Top">
+        <i class="fa fa-arrow-circle-up fa-2x" aria-hidden="true"></i>
+      </a>
+    </button>
+    <!-- ******************************* -->   
+
+    <!-- JAVASCRIPT ******************** --> 
+    <!-- \\\\ Scroll-to-Top   ********** -->
     <script src="inc/scrollTop.js"></script>
-    <!-- \\\\\\ Font-Awesome Icons -->
+    <!-- \\\\ Font-Awesome Icons ******* -->
     <script src="https://use.fontawesome.com/ceeedfe13b.js"></script>
-    <!-- \\\\\\ Reportcard JS -->
+    <!-- \\\\ Reportcard JS    ********* -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
     <script src="js/reportcard/js/reportcard.js"></script>
     <script>$('.report-card.treehouse').reportCard({
             userName: 'mistermoody',
             site: 'treehouse',
             badgesAmount: 7});</script>
-    <!-- ************************* --> 
+    <!-- ******************************* -->   
   </body>
 </html>
