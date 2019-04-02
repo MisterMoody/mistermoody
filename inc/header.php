1d980199b580c2475370cde60abe2750099adb68
MisterMoody <principal@mistermoody.com>
2019-04-02 17:41:58 -0400

    Amalgamate <head> with Overhauled HTML

--- inc/header.php
+++ inc/header.php
@@ -2,61 +2,39 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="width=device-width; initial-scale=1.0">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>&Mopf;ister &Mopf;oody</title>
-    <meta name='description' content='Mister Moody: Edupreneur based in Louisville Kentucky' />
-    <meta name="keywords" content="Edupreneur, Teacher, Tutor, Virtual Assistant, Web Designer, TEFL, ESL" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0;">
+    <title>Mister Moody</title>
+    <meta name='description' content="Official Site for Mister Moody" />
+    <meta name="keywords" content="" />
     <!-- Browser Tab Icon -->
     <link rel="shortcut icon" href="img/me.jpg" type="img/m" class="rounded">    
-    <!-- Custom CSS Styles -->   
-    <link rel="stylesheet" href="css/main.css">
+    <!-- CSS Styles -->   
     <link rel="stylesheet" href="css/grid.css">
-    <!--<link rel="stylesheet" href="css/assets.css">
-    <link rel="stylesheet" href="css/animations.css">
-    <link rel="stylesheet" href="css/minified.css">-->
-    <!-- 
-    <link rel="stylesheet" href="css/animations.css">-->
     <!-- Custom Fonts -->       
-	  <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Raleway" rel="stylesheet">
-    <!-- FontAwesome CSS Fonts -->
+	  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bree+Serif|Raleway">
+    <!-- FontAwesome Fonts -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Treehouse ReportCard by Riley Hilliard -->
+    <!-- Treehouse 'Badges' ReportCard by Riley Hilliard -->
     <link href="js/reportcard/css/badges.css" rel="stylesheet">
   </head>
   <body>
-  <!-- ********************************************************* -->
-  <!-- ********************************************************* -->
-
-		<!-- //'start of' MAIN GRID CONTAINER-->	
-    <article> 
-      <!---->   
-      <!-- //NAVIGATION -->
-      <!---->
-			<nav class="navbar">
-        <span class="navbar-toggle" id="js-navbar-toggle">
-          <i class="fa fa-bars"></i>
-        </span>
-        <a href="index" class="logo">Mister Moody</a>
-        <ul class="main-nav" id="js-menu">
-            <li>
-                <a href="about" class=" active nav-links">About</a>
-            </li>
-            <li>
-                <a href="work" class="nav-links">Work</a>
-            </li>
-            <li>
-                <a href="contact" class="nav-links">Contact</a>
-            </li>
-            <!--<li>
-                <a href="devops.php" class="nav-links">Dev Ops</a>
-            </li>
-            <li>
-                <a href="articles.php" class="nav-links">Articles</a>
-            </li>-->
-        </ul>
-      </nav>
-      <!---->
-      <!-- //CONTENT-BEGINS..... -->
-      <!---->
+  <!-- ********************************* -->
+  <!-- ** // GRID CONTAINER 'start' **** -->
+  <!-- ********************************* -->
+    <article class="indexGrid">    
       
+      <!--  // HEADER ( Nav / Logo ) *** -->
+      <header>
+        <div class="header__logo"><h1><a href="index">Mister Moody</a></h1></div>
+          <ul class="header__title">
+            <li><a href="index">About</a></li>
+            <li><a href="portfolio">Portfolio</a></li>
+            <li><a href="contact">Contact</a></li>
+          </ul>
+      </header>
+      <!-- ***************************** -->   
+            
+      <!-- ***************************** -->
+      <!-- // CONTENT-BEGINS............ -->
+      <!-- ***************************** -->
