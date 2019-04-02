cfca3e6a7b64b7fd59cb1257c28f826d33b90868
MisterMoody <principal@mistermoody.com>
2019-04-02 17:50:11 -0400

    Add Web Design Resources Page

---
+++ webdesign.php
@@ -0,0 +1,86 @@
+<?php
+$pageTitle = "Portfolio Item: Web Design";
+include ("inc/header.php");
+?>
+<!-- ************************************************** -->
+<!-- ************* <<PORTFOLIO GALLERY>>  ************* -->
+
+<main>
+<!-- //CONTENT-BEGINS..... -->
+    <h2>Web Design</h2>
+    <p style="text-align: center;">Useful resources for front-end web design and project management.</p>
+  <!-- HTML -->
+  <section>
+    <h3>HTML</h3>
+    <p>These resources are useful for referencing Hypertext Markup Language elements in order to create semantic layouts. Learn more about <a class="resourceText" target="_blank" href="https://mistermoody.github.io/usingHTML.html">HTML</a>, which is the standard markup language for creating web pages.</p>
+    <div class="resourceLink">
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+        
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+          
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+          
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+    </div>
+  </section>  
+  <!-- CSS -->
+  <section>
+    <h3>CSS</h3>
+    <p>These resources are useful for referencing Cascading Style Sheet syntax to create declarations that control the presentation of HTML elements. Learn more about <a class="resourceText" target="_blank" href="https://mistermoody.github.io/usingCSS.html">CSS</a>, which is the standard programming language used to make websites 'pretty.'</p>
+    <div class="resourceLink">
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+        
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+          
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+          
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+    </div>
+  </section>
+  <!-- Project Management -->
+  <section>
+    <h3>Project Management</h3>
+    <p>These resources are useful for managing projects insofar as the front-end is concerned. Learn more about ways to <a class="resourceText" target="_blank" href="https://mistermoody.github.io/management.html">manage a project</a>.</p>
+    <div class="resourceLink">
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+        
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+          
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+          
+      <span>Content Title + Link</span>
+      <span>[Link]</span>
+    </div>
+  </section>
+  <!-- -->
+  <br>
+  <!-- -->
+  <section></section>
+<!-- .....CONTENT-ENDS// --> 
+</main>
+
+<!-- ************************************************** -->
+<!-- ************************************************** -->
+<?php
+include ("inc/footer.php"); 
+?>
