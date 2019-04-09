a274e6bd92a249ba4829fb60b40b41d91d29ead7
MisterMoody <principal@mistermoody.com>
2019-04-09 16:00:27 -0400

    Add Content for Resume 'Training'

--- resume.php
+++ resume.php
@@ -7,11 +7,120 @@ include ("inc/header.php");
 
 <main>
 <!-- //CONTENT-BEGINS..... -->
+  
+  <article class="resumeGrid">
+    <section class="resume__author">
+      <div class="resume__title">
+        <div>Ray Moody</div>
+        <div class="res__sectionTitle">Web Developer</div>
+      </div>
+      <div class="resume__contact">
+        <ul>
+          <li>&#40;502&#41; 558&#45;9240</li>
+          <li>principal&#64;mistermoody&#46;com</li>
+          <li>Louisville, Kentucky 40211</li>
+        </ul>
+      </div>
+    </section>
+    <!-- -->
+    <section class="resume__background">
+      <div class="res__sectionTitle">Background</div>
+      <p>Non&#45;traditional educator and web technology enthusiast exploring front-end web developer opportunities. Self-directive learner with a full-stack mentality capable of designing, developing and deploying a website to specification. Results&#45;oriented, hands&#45;on professional with a distinctinve cosmopolitan experience delievering proficient customer and language acquisition services. </p>
+    </section>
+    <!-- -->
+    <section class="resume__work">
+      <div class="res__sectionTitle">Work Experience</div>
+        <p>Nunc eget faucibus suspendisse ut imperdiet tellus. Vivamus metus leo, molestie et nec felis.  Pellentesque habitant morbi tristique senectus turpis egestas.  Maecenas vel turpis id nisl porttitor tincidunt.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget faucibus lorem. Suspendisse ut imperdiet tellus. Vivamus metus leo, molestie et molestie vitae, iaculis nec felis. In molestie euismod imperdiet.</p>
+      <!--<div class="res__work__Card">
+        <div class="res__work__JobEmployer">
+          
+        </div>
+        <div class="res__work__DateLocation">
+          
+        </div>
+        <div class="res__work__Responsibilities">
+          
+        </div>
+      </div>-->
+    </section>
+    <!-- -->
+    <section class="resume__skills">
+      <div>
+        <div class="res__sectionTitle">Skills</div>
+        <p>Aliquam ullamcorper sapien nec felis tempor, in viverra augue euismod. Maecenas vel turpis id nisl porttitor tincidunt. In molestie euismod imperdiet. </p>
+      </div>
+    </section>
+    <!-- -->
+    <section class="resume__education">
+      <div>
+        <div class="res__sectionTitle">Training</div>
+      <div class="resume__Card">
+        <div class="res__work__Employment">
+          <div class="res__work__jobTitle"><span>C.C.</span> Web Development Cohort</div>
+          <div class="res__work__jobEmployer">&#60;Code&#62; Louisville</div>
+          <div class="res__work__jobDate">1&#47;2015 &#45; 6&#47;2015</div>
+          <div class="res__work__jobLocation">Louisville, KY</div>
+        </div>
+        <div class="res__work__Responsibilities">
+          <ol>
+            <li>Duis commodo ante ac bibendum placerat ligula</li>
+            <li>Pellentesque habitant morbi tristique senectus</li>
+            <li>Rutrum nisi, eget molestie ligula sapien ac mauris et netus et malesuada fames ac turpis egestas.</li>
+          </ol>
+        </div>
+      </div>
+      <div class="resume__Card">
+        <div class="res__work__Employment">
+          <div class="res__work__jobTitle"><span>C.C.</span>Microsoft Office Specialist</div>
+          <div class="res__work__jobEmployer">Microsoft</div>
+          <div class="res__work__jobDate">2&#47;2010</div>
+          <div class="res__work__jobLocation">Online</div>
+        </div>
+        <div class="res__work__Responsibilities">
+          <ol>
+            <li>Demonstrated proficiency using the Microsoft Office Suite</li>
+          </ol>
+        </div>
+      </div>
+      <div class="resume__Card">
+        <div class="res__work__Employment">
+          <div class="res__work__jobTitle"><span>C.C.</span> TEFL</div>
+          <div class="res__work__jobEmployer">ITTT Inc.</div>
+          <div class="res__work__jobDate">5&#47;2007 &#45; 7&#47;2007</div>
+          <div class="res__work__jobLocation">Thailand</div>
+        </div>
+        <div class="res__work__Responsibilities">
+          <ol>
+            <li>Completed 100&#45;hour curriculum on topics that included classroom management, lesson planning and teaching English grammar to non-native speakers.</li>
+          </ol>
+        </div>
+      </div>
+      <div class="resume__Card">
+        <div class="res__work__Employment">
+          <div class="res__work__jobTitle"><span>B.A.</span> Political Science</div>
+          <div class="res__work__jobEmployer">the University of Louisville</div>
+          <div class="res__work__jobDate">1&#47;2000 &#45; 5&#47;2003</div>
+          <div class="res__work__jobLocation">Louisville, KY</div>
+        </div>
+        <div class="res__work__Responsibilities">
+          <ol>
+            <li>Chapter President: Pi Sigma Alpha</li>
+            <li>Student Assistant: Office of the President</li>
+            <li>Intern: Legislative Research Commission &#40;Office of Budget Review&#41;</li>
+            <li>Co&#45;Op: The Econ Village Group, LLC </li>
+            <li>Volunteer Tutor: Louisville Urban League</li>
+          </ol>
+        </div>
+      </div>
+      </div>
+    </section>
+    <!-- -->
+  </article>
 
 
-
-</main>
-<!-- .....CONTENT-ENDS// -->  
+<!-- .....CONTENT-ENDS// --> 
+</main> 
 
 
 <!-- ************************************************** -->
