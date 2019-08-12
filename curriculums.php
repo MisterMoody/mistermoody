33c153430965278642d40779858adf206d17807c
MisterMoody <principal@mistermoody.com>
2019-08-12 19:22:57 -0400

    Add Grid Layout for Curriculum Topics

--- curriculums.php
+++ curriculums.php
@@ -8,7 +8,23 @@ include ("inc/header.php");
 
 <!-- //CONTENT-BEGINS..... -->
 <main>
-  <h1 style="height: 85vh;">Coming Soon</h1>
+  <!--<h1 style="height: 85vh;">Coming Soon</h1>-->
+  <!-- CURRICULUM "TOPICS"  -->
+  <article class="cur__TopicGrid">
+    <section class="cur__TopicStyle">
+      <h1>Web Dev Starter Kit</h1>
+    </section>
+    <section class="cur__TopicStyle">
+      <h1>Language Arts</h1>
+    </section>
+    <section class="cur__TopicStyle">
+      <h1>STEM Math</h1>
+    </section>
+    <section class="cur__TopicStyle">
+      <h1>Digital Citizenship</h1>
+    </section>
+  </article>
+  
 </main>
 <!-- .....CONTENT-ENDS// --> 
 
