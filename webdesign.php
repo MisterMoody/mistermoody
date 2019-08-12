ce1b3c5cddc212c3dbd1f575ba11629996dcbf10
MisterMoody <principal@mistermoody.com>
2019-08-12 19:22:40 -0400

    Grammar Edits

--- webdesign.php
+++ webdesign.php
@@ -10,30 +10,6 @@ include ("inc/header.php");
 <main>
   <h1 style="height: 85vh;">Coming Soon</h1>
 
-  <!--<section>
-    <h1>Web Design 101</h1>
-    <p>
-      Hypertext Markup Language is the standard markup language for creating semantic layouts for websites. HTML uses elements (i.e. tags) that allows designers to organize the content of a web page. Although there are dozens of elements that can be used, all elements are either semantic or not. Semantic elements clearly defines content for the browser and is used to provide structure for the overall layout for the content of a web page. Such elements always begin on a new line on a web page, extending the full-width of the horizontal space of its parent element (i.e. container).
-    </p>
-    <p>
-      Inline-level elements, on the other hand, reside within semantic elements, only takes up as much space as is required by its content and cannot start on a new line. Such elements are helpful in that they emphasize how content will be displayed, particularly when CSS is applied to them.
-    </p>
-    <p>
-      HTML5 no longer defines elements as <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Block-level_elements" target="_blank" class="resourceText">block</a> or <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Inline_elements" target="_blank" class="resourceText">inline</a> and suggests that designers use <a href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Content_categories" target="_blank" class="resourceText">content categories</a> when marking up a web page. The resources presented below reference HTML elements (i.e. tags) as well as attributes that can be used to properly structure content while providing accessibility which makes websites more inclusive.
-    </p>
-    <p>
-      A Cascading Style Sheet is a document with instructions that are called <em>statements</em>, which defines the visual design of HTML elements. All statements begin with a <em>selector</em> and is followed by a set of curly braces; these target specific and/or groups of elements. Within the statement are <em>property-value pairs</em>, which declares a specific styling, hence its name: declaration. 
-    </p>
-    <p>
-      A major tenet of writing CSS is to keep the syntax DRY: that is, do not repeat yourself. This can be accomplished by following design principles that conform to web standards such as outlining a grid layout for responsiveness; building an interactively-accessible form; or properly designating colors, fonts, images, icons and navigation links.
-      Learn more about <a class="resourceText" target="_blank" href="https://mistermoody.github.io/usingCSS.html">CSS</a>, which is the standard programming language used to make websites pretty.
-    </p>
-    <div>
-      <p>
-        Blogged Resources: <a class="resourceText" target="_blank" href="https://mistermoody.github.io/usingHTML.html">HTML</a> -- <a class="resourceText" target="_blank" href="https://mistermoody.github.io/usingCSS.html">CSS</a> -- <a class="resourceText" target="_blank" href="https://mistermoody.github.io/management.html">Project Management</a>
-      </p>
-    </div>
-  </section>-->
 </main>
 <!-- .....CONTENT-ENDS// --> 
 
