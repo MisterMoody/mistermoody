445b8b760eb6ba0346b1d332b2dc55395e32cb8d
MisterMoody <principal@mistermoody.com>
2019-08-14 13:45:04 -0400

    Add 'Modals' to TopicStyle Section for Topic 'Description'

--- curriculums.php
+++ curriculums.php
@@ -1,5 +1,5 @@
 <?php
-$pageTitle = "Mister Moody ASSETS";
+$pageTitle = "Mister Moody CURRICULUMS";
 include ("inc/header.php");
 ?>
 <!-- ******************************************************* -->
@@ -13,26 +13,57 @@ include ("inc/header.php");
   <article class="cur__TopicGrid">
     <section class="cur__TopicStyle">
       <div class="code">
-      <h1>Web Dev Starter Kit</h1>
+        <h1>Web Dev Starter Kit</h1>
+        <div id="overlayWebDev" onclick="clickOffWebDev()">
+          <div id="textWebDev">
+            <p>
+              WEB DEV STARTER KIT ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur purus ut faucibus pulvinar elementum integer enim. Sapien eget mi proin sed libero. <b>a 250 word Para</b> Nullam non nisi est sit amet facilisis magna etiam tempor. Sodales ut etiam sit amet. Proin sed libero enim sed faucibus turpis in eu mi. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Vulputate eu scelerisque felis imperdiet proin fermentum. Nec nam aliquam sem et tortor. Lacus viverra vitae congue eu consequat. Turpis egestas integer eget aliquet nibh praesent tristique. Feugiat sed lectus vestibulum mattis ullamcorper. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Facilisi cras fermentum odio eu feugiat pretium nibh. Pulvinar mattis nunc sed blandit libero. "C'est la vie~ 4 da win on dat reelnegusish!"
+            </p>
+          </div>
+        </div>  
       </div>
+      <button class="modalBTN" onclick="clickOnWebDev()">Learn More</button>
     </section>
     <section class="cur__TopicStyle">
       <div class="language">
         <h1>Language <span class="variant">for</span> Artists</h1>
+        <div id="overlayLangArts" onclick="clickOffLangArts()">
+          <div id="textLangArts">
+            <p>
+              LANGUAGE for ARTISTS ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur purus ut faucibus pulvinar elementum integer enim. Sapien eget mi proin sed libero. <b>a 250 word Para</b> Nullam non nisi est sit amet facilisis magna etiam tempor. Sodales ut etiam sit amet. Proin sed libero enim sed faucibus turpis in eu mi. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Vulputate eu scelerisque felis imperdiet proin fermentum. Nec nam aliquam sem et tortor. Lacus viverra vitae congue eu consequat. Turpis egestas integer eget aliquet nibh praesent tristique. Feugiat sed lectus vestibulum mattis ullamcorper. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Facilisi cras fermentum odio eu feugiat pretium nibh. Pulvinar mattis nunc sed blandit libero. "C'est la vie~ 4 da win on dat reelnegusish!"
+            </p>
+          </div>
+        </div>  
       </div>
+      <button class="modalBTN" onclick="clickOnLangArts()">Learn More</button>
     </section>
     <section class="cur__TopicStyle">
       <div class="math">
         <h1>STEM Mathematics</h1>
+        <div id="overlaySTEM" onclick="clickOffSTEM()">
+          <div id="textSTEM">
+            <p>
+              STEM Math ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur purus ut faucibus pulvinar elementum integer enim. Sapien eget mi proin sed libero. <b>a 250 word Para</b> Nullam non nisi est sit amet facilisis magna etiam tempor. Sodales ut etiam sit amet. Proin sed libero enim sed faucibus turpis in eu mi. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Vulputate eu scelerisque felis imperdiet proin fermentum. Nec nam aliquam sem et tortor. Lacus viverra vitae congue eu consequat. Turpis egestas integer eget aliquet nibh praesent tristique. Feugiat sed lectus vestibulum mattis ullamcorper. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Facilisi cras fermentum odio eu feugiat pretium nibh. Pulvinar mattis nunc sed blandit libero. "C'est la vie~ 4 da win on dat reelnegusish!"
+            </p>
+          </div>
+        </div>  
       </div>
+      <button class="modalBTN" onclick="clickOnSTEM()">Learn More</button>
     </section>
     <section class="cur__TopicStyle">
       <div class="id-card">
         <h1>Digital Citizenship</h1>
-        </div>
+        <div id="overlayDigiCitizen" onclick="clickOffDigiCitizen()">
+          <div id="textDigiCitizen">
+            <p>
+              DIGITAL CITIZENSHIP ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur purus ut faucibus pulvinar elementum integer enim. Sapien eget mi proin sed libero. <b>a 250 word Para</b> Nullam non nisi est sit amet facilisis magna etiam tempor. Sodales ut etiam sit amet. Proin sed libero enim sed faucibus turpis in eu mi. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Vulputate eu scelerisque felis imperdiet proin fermentum. Nec nam aliquam sem et tortor. Lacus viverra vitae congue eu consequat. Turpis egestas integer eget aliquet nibh praesent tristique. Feugiat sed lectus vestibulum mattis ullamcorper. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Facilisi cras fermentum odio eu feugiat pretium nibh. Pulvinar mattis nunc sed blandit libero. "C'est la vie~ 4 da win on dat reelnegusish!"
+            </p>
+          </div>
+        </div>  
+      </div>
+      <button class="modalBTN" onclick="clickOnDigiCitizen()">Learn More</button>
     </section>
   </article>
-  
 </main>
 <!-- .....CONTENT-ENDS// --> 
 
