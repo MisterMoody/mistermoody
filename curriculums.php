e005c29b951c18dc23dff39c65c5ee6623b2cc7c
MisterMoody <principal@mistermoody.com>
2019-08-15 18:42:41 -0400

    Modify Modal Function Exit Strategy: User must click btn to exit

--- curriculums.php
+++ curriculums.php
@@ -8,16 +8,19 @@ include ("inc/header.php");
 
 <!-- //CONTENT-BEGINS..... -->
 <main>
-  <!--<h1 style="height: 85vh;">Coming Soon</h1>-->
   <!-- CURRICULUM "TOPICS"  -->
   <article class="cur__TopicGrid">
     <section class="cur__TopicStyle">
       <div class="code">
         <h1>Web Dev Starter Kit</h1>
-        <div id="overlayWebDev" onclick="clickOffWebDev()">
+        <div id="overlayWebDev">
           <div id="textWebDev">
+            <i class="fa fa-times" onclick="clickOffWebDev()"></i>
             <p>
-              WEB DEV STARTER KIT ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur purus ut faucibus pulvinar elementum integer enim. Sapien eget mi proin sed libero. <b>a 250 word Para</b> Nullam non nisi est sit amet facilisis magna etiam tempor. Sodales ut etiam sit amet. Proin sed libero enim sed faucibus turpis in eu mi. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Vulputate eu scelerisque felis imperdiet proin fermentum. Nec nam aliquam sem et tortor. Lacus viverra vitae congue eu consequat. Turpis egestas integer eget aliquet nibh praesent tristique. Feugiat sed lectus vestibulum mattis ullamcorper. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Facilisi cras fermentum odio eu feugiat pretium nibh. Pulvinar mattis nunc sed blandit libero. "C'est la vie~ 4 da win on dat reelnegusish!"
+              WEB DEV STARTER KIT ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur purus ut faucibus pulvinar elementum integer enim. Sapien eget mi proin sed libero. <b>a 250 word Para</b> Nullam non nisi est sit amet facilisis magna etiam tempor. Sodales ut etiam sit amet. 
+            </p>
+            <p>
+              Proin sed libero enim sed faucibus turpis in eu mi. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Vulputate eu scelerisque felis imperdiet proin fermentum. Nec nam aliquam sem et tortor. Lacus viverra vitae congue eu consequat. Turpis egestas integer eget aliquet nibh praesent tristique. Feugiat sed lectus vestibulum mattis ullamcorper. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Facilisi cras fermentum odio eu feugiat pretium nibh. Pulvinar mattis nunc sed blandit libero. "C'est la vie~ 4 da win on dat reelnegusish!"
             </p>
           </div>
         </div>  
@@ -27,10 +30,14 @@ include ("inc/header.php");
     <section class="cur__TopicStyle">
       <div class="language">
         <h1>Language <span class="variant">for</span> Artists</h1>
-        <div id="overlayLangArts" onclick="clickOffLangArts()">
+        <div id="overlayLangArts">
           <div id="textLangArts">
+            <i class="fa fa-times" onclick="clickOffLangArts()"></i>
             <p>
-              LANGUAGE for ARTISTS ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur purus ut faucibus pulvinar elementum integer enim. Sapien eget mi proin sed libero. <b>a 250 word Para</b> Nullam non nisi est sit amet facilisis magna etiam tempor. Sodales ut etiam sit amet. Proin sed libero enim sed faucibus turpis in eu mi. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Vulputate eu scelerisque felis imperdiet proin fermentum. Nec nam aliquam sem et tortor. Lacus viverra vitae congue eu consequat. Turpis egestas integer eget aliquet nibh praesent tristique. Feugiat sed lectus vestibulum mattis ullamcorper. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Facilisi cras fermentum odio eu feugiat pretium nibh. Pulvinar mattis nunc sed blandit libero. "C'est la vie~ 4 da win on dat reelnegusish!"
+              LANGUAGE ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur purus ut faucibus pulvinar elementum integer enim. Sapien eget mi proin sed libero. <b>a 250 word Para</b> Nullam non nisi est sit amet facilisis magna etiam tempor. Sodales ut etiam sit amet. 
+            </p>
+            <p>
+              Proin sed libero enim sed faucibus turpis in eu mi. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Vulputate eu scelerisque felis imperdiet proin fermentum. Nec nam aliquam sem et tortor. Lacus viverra vitae congue eu consequat. Turpis egestas integer eget aliquet nibh praesent tristique. Feugiat sed lectus vestibulum mattis ullamcorper. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Facilisi cras fermentum odio eu feugiat pretium nibh. Pulvinar mattis nunc sed blandit libero. "C'est la vie~ 4 da win on dat reelnegusish!"
             </p>
           </div>
         </div>  
@@ -40,10 +47,14 @@ include ("inc/header.php");
     <section class="cur__TopicStyle">
       <div class="math">
         <h1>STEM Mathematics</h1>
-        <div id="overlaySTEM" onclick="clickOffSTEM()">
+        <div id="overlaySTEM">
           <div id="textSTEM">
+            <i class="fa fa-times" onclick="clickOffSTEM()"></i>
             <p>
-              STEM Math ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur purus ut faucibus pulvinar elementum integer enim. Sapien eget mi proin sed libero. <b>a 250 word Para</b> Nullam non nisi est sit amet facilisis magna etiam tempor. Sodales ut etiam sit amet. Proin sed libero enim sed faucibus turpis in eu mi. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Vulputate eu scelerisque felis imperdiet proin fermentum. Nec nam aliquam sem et tortor. Lacus viverra vitae congue eu consequat. Turpis egestas integer eget aliquet nibh praesent tristique. Feugiat sed lectus vestibulum mattis ullamcorper. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Facilisi cras fermentum odio eu feugiat pretium nibh. Pulvinar mattis nunc sed blandit libero. "C'est la vie~ 4 da win on dat reelnegusish!"
+              STEM Mathematics ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur purus ut faucibus pulvinar elementum integer enim. Sapien eget mi proin sed libero. <b>a 250 word Para</b> Nullam non nisi est sit amet facilisis magna etiam tempor. Sodales ut etiam sit amet. 
+            </p>
+            <p>
+              Proin sed libero enim sed faucibus turpis in eu mi. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Vulputate eu scelerisque felis imperdiet proin fermentum. Nec nam aliquam sem et tortor. Lacus viverra vitae congue eu consequat. Turpis egestas integer eget aliquet nibh praesent tristique. Feugiat sed lectus vestibulum mattis ullamcorper. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Facilisi cras fermentum odio eu feugiat pretium nibh. Pulvinar mattis nunc sed blandit libero. "C'est la vie~ 4 da win on dat reelnegusish!"
             </p>
           </div>
         </div>  
@@ -53,10 +64,14 @@ include ("inc/header.php");
     <section class="cur__TopicStyle">
       <div class="id-card">
         <h1>Digital Citizenship</h1>
-        <div id="overlayDigiCitizen" onclick="clickOffDigiCitizen()">
+        <div id="overlayDigiCitizen">
           <div id="textDigiCitizen">
+            <i class="fa fa-times" onclick="clickOffDigiCitizen()"></i>
             <p>
-              DIGITAL CITIZENSHIP ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur purus ut faucibus pulvinar elementum integer enim. Sapien eget mi proin sed libero. <b>a 250 word Para</b> Nullam non nisi est sit amet facilisis magna etiam tempor. Sodales ut etiam sit amet. Proin sed libero enim sed faucibus turpis in eu mi. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Vulputate eu scelerisque felis imperdiet proin fermentum. Nec nam aliquam sem et tortor. Lacus viverra vitae congue eu consequat. Turpis egestas integer eget aliquet nibh praesent tristique. Feugiat sed lectus vestibulum mattis ullamcorper. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Facilisi cras fermentum odio eu feugiat pretium nibh. Pulvinar mattis nunc sed blandit libero. "C'est la vie~ 4 da win on dat reelnegusish!"
+              DIGITAL CITIZENSHIP ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur purus ut faucibus pulvinar elementum integer enim. Sapien eget mi proin sed libero. <b>a 250 word Para</b> Nullam non nisi est sit amet facilisis magna etiam tempor. Sodales ut etiam sit amet. 
+            </p>
+            <p>
+              Proin sed libero enim sed faucibus turpis in eu mi. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Vulputate eu scelerisque felis imperdiet proin fermentum. Nec nam aliquam sem et tortor. Lacus viverra vitae congue eu consequat. Turpis egestas integer eget aliquet nibh praesent tristique. Feugiat sed lectus vestibulum mattis ullamcorper. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Facilisi cras fermentum odio eu feugiat pretium nibh. Pulvinar mattis nunc sed blandit libero. "C'est la vie~ 4 da win on dat reelnegusish!"
             </p>
           </div>
         </div>  
