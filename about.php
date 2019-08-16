c79009ce2627241f9c79248b9885001d0f414919
MisterMoody <principal@mistermoody.com>
2019-08-16 12:10:48 -0400

    Restore Missing Content

--- about.php
+++ about.php
@@ -6,7 +6,43 @@ include ("inc/header.php"); //Contains 'metadata' and '<navigation> element'
 <!-- ******************** <<HOMEPAGE>>  *************** -->
 
 <main>
-  <h1 style="height: 85vh;">Coming Soon</h1>
+  
+  <!-- #About (Introduction) -->
+  <section>
+    <p>Non-traditional educator with a plethora of experiences delivering language acquisition services for private and public institutions of education in China, Japan, South Korea and the United States.</p>
+    <picture><img src="img/MMgrey.jpg" class="mainImage" alt="Mister Moody Logo"/></picture>
+    <p id="about">Self-taught web implementation specialist able to proficiently design, develop and deploy a web application to specification.</p>
+    <p><img src="img/tailorVanilla.jpg" class="imgL" alt="Sewing in a Chinese Factory"/>As a designer, I can adapt a low-fidelity prototype into a viable website using HTML, CSS, JavaScript, content management tools and digital media. As a developer, I utilize the PHP scripting language that incorporates object-oriented programming practices to integrate server-side functionality into a web application. Vital components of this process entails auscultating user-experience; amalgamating accessibility, performance requirements and other technical considerations; HTTP server installation; database configuration; and using the SSH protocol in conjunction with version control to provide secure remote access to mitigate maintenance concerns. To this end, I am currently exploring opportunities by which I can leverage my skillset to pursue a position in the tech industry.</p>
+  </section>
+  <!-- -->
+  <!-- #Contact -->
+  <section>
+    <h4>Get In Touch</h4>
+    <p class="readText">I am on the web. Check out my work on Github, follow me on Twitter, download my resume. Heck, give me a call or text me an email. Do it now!</p>
+    <ul class="social">
+      <li>
+        <a class="github" href="https://github.com/MisterMoody" target="_blank" title="Check Out My Work on Github">
+          <i class="fa fa-github-alt fa-2x" aria-hidden="true"></i></a>
+      </li>
+      <li>
+        <a class="twitter" href="https://twitter.com/Mister_Moody" target="_blank" title="Follow Me on Twitter">
+          <i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+      </li>
+      <li>
+        <a class="download" href="moodyCV.doc" download="moodyCV.doc" title="Download Resume">
+          <i class="fa fa-download fa-2x" aria-hidden="true"></i></a>
+      </li>
+      <li>
+        <a class="phone" href="tel:+15025589240" title="Call Me">
+          <i class="fa fa-mobile fa-2x" aria-hidden="true"></i></a>
+      </li>
+      <li>
+        <a class="email" href="mailto:raymoody10@gmail.com" title="Email Me">
+          <i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a>
+      </li>
+    </ul>
+  </section>
+  <!-- -->
 </main>
 
 <!-- ************************************************** -->
