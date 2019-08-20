e9c365dce913441e836b9df2014a72bcc8490854
MisterMoody <principal@mistermoody.com>
2019-08-19 23:01:59 -0400

    Move Content from Work.php to About.php

--- about.php
+++ about.php
@@ -14,35 +14,134 @@ include ("inc/header.php"); //Contains 'metadata' and '<navigation> element'
     <p id="about">Self-taught web implementation specialist able to proficiently design, develop and deploy a web application to specification.</p>
     <p><img src="img/tailorVanilla.jpg" class="imgL" alt="Sewing in a Chinese Factory"/>As a designer, I can adapt a low-fidelity prototype into a viable website using HTML, CSS, JavaScript, content management tools and digital media. As a developer, I utilize the PHP scripting language that incorporates object-oriented programming practices to integrate server-side functionality into a web application. Vital components of this process entails auscultating user-experience; amalgamating accessibility, performance requirements and other technical considerations; HTTP server installation; database configuration; and using the SSH protocol in conjunction with version control to provide secure remote access to mitigate maintenance concerns. To this end, I am currently exploring opportunities by which I can leverage my skillset to pursue a position in the tech industry.</p>
   </section>
-  <!-- -->
-  <!-- #Contact -->
+  
+  <hr>
+  
+  <!-- #WORK -->
   <section>
-    <h4>Get In Touch</h4>
-    <p class="readText">I am on the web. Check out my work on Github, follow me on Twitter, download my resume. Heck, give me a call or text me an email. Do it now!</p>
-    <ul class="social">
-      <li>
-        <a class="github" href="https://github.com/MisterMoody" target="_blank" title="Check Out My Work on Github">
-          <i class="fa fa-github-alt fa-2x" aria-hidden="true"></i></a>
-      </li>
-      <li>
-        <a class="twitter" href="https://twitter.com/Mister_Moody" target="_blank" title="Follow Me on Twitter">
-          <i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
-      </li>
-      <li>
-        <a class="download" href="moodyCV.doc" download="moodyCV.doc" title="Download Resume">
-          <i class="fa fa-download fa-2x" aria-hidden="true"></i></a>
-      </li>
-      <li>
-        <a class="phone" href="tel:+15025589240" title="Call Me">
-          <i class="fa fa-mobile fa-2x" aria-hidden="true"></i></a>
-      </li>
-      <li>
-        <a class="email" href="mailto:raymoody10@gmail.com" title="Email Me">
-          <i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a>
-      </li>
-    </ul>
-  </section>
-  <!-- -->
+    <!-- Teaching Experience -->
+    <div>
+      <p>
+        I am currently employed as a certified substitute teacher with <a href="https://jefferson.kyschools.us/" target="_blank">Jefferson County Public Schools</a> in my hometown of Louisville, Kentucky. In my spare time, I tinker with web technologies in an effort to develop my sole-proprietorship. I kindly invite you to explore the content on this page to learn more about my professional experiences and how we could possibly collaborate on a project.
+      </p>
+      <p>
+        I put together a table of useful <a class="resourceText" href="webdesign" alt="Link to Resources" target="_blank" title="Online Resources">Online Resources</a> that help me build websites, which is a supplement to this <a class="resourceText" href="https://mistermoody.github.io/" alt="Link to Blog" target="_blank" title="Blog of Knowledge">DIY Guide</a> for creating a website.
+      </p>
+      <details>
+        <summary>Teaching Experience &nbsp;<code>9,000+ Hours</code></summary>
+        <p>Below is a list of some of the educational institutions that I have collaborated with:</p>
+        <div class="trainingGrid">
+          <span>2018 - <small><i>Present</i></small> </span>
+          <span>Substitute Teacher <b>&#64;</b> Jefferson Co. Public Schools <small>(KY)</small></span>
+          <span>2016 - 2017 </span>
+          <span>TEFL Instructor <b>&#64;</b> Jiaxing College</span>
+          <span>2012 - 2013 </span>
+          <span>TEFL Instructor <b>&#64;</b> Zhejiang Middle School</span>
+          <span>2007 - 2009 </span>
+          <span>TEFL Trainer <b>&#64;</b> Oxford Seminars</span>
+          <span>2005 - 2006 </span>
+          <span>TEFL Instructor <b>&#64;</b> Joongdong High School</span>
+          <span>2004 - 2006 </span>
+          <span>TEFL Consultant <b>&#64;</b> US-Japan Edu. Exchange Program</span>
+          <span>2003 - 2004 </span>
+          <span>TEFL Instructor <b>&#64;</b> Sogang University</span>
+          <span>2000 - 2001 </span>
+          <span>Rec. Supervisor <b>&#64;</b> Cabbage Patch Settlement House</span>
+        </div> 
+      </details>
+    </div>
+    <p>
+      Jack-of-All-Trades, I am not. However, I am an effective communicator with a distinctive international experience capable of doing anything that I put my mind to.
+    </p>
+    <!-- Education + Skills -->
+    <div class="subGrid">
+      <!-- Education -->
+      <div>
+        <div class="grid-table">
+          <span>2015</span>
+          <span>Web Dev Training</span>
+          <span>USA</span>
+          <span>Treehouse</span>
+
+          <span>2011</span>
+          <span>Mandarin Language Studies</span>
+          <span>China</span>
+          <span>Shanghai Ocean University</span>
+
+          <span>2010</span>
+          <span>MOS Training</span>
+          <span>USA</span>
+          <span>Microsoft</span>
+
+          <span>2007</span>
+          <span>TEFL Teacher Training</span>
+          <span>Thailand</span>
+          <span class="smallSpan">International TEFL Teacher Training, Inc.</span>
+
+          <span>2003</span>
+          <span>B.A. Political Science</span>
+          <span>USA</span>
+          <span>University of Louisville</span>
+        </div>
+      </div>
+      <!-- Skills -->
+      <div>
+        <div class="grid-row">
+          <div class="SkillBar">
+            <div id="Skill-Design">
+              <span class="Skill-Area">Design</span> <span class="PercentText">80%</span>
+            </div>
+          </div>
+          <div class="skillSpecs">
+            <span>HTML5 | CSS3 | IA/UI/UX | Accessibility</span>
+          </div>
+        </div>
+        <div class="grid-row">
+          <div class="SkillBar">
+            <div id="Skill-Develop">
+              <span class="Skill-Area">Develop</span> <span class="PercentText">68%</span>
+            </div>
+          </div>
+          <div class="skillSpecs">
+            <span>PHP7 | Composer | JavaScript | npm</span>
+          </div>
+        </div>
+        <div class="grid-row">
+          <div class="SkillBar">
+            <div id="Skill-Deploy">
+              <span class="Skill-Area">Deploy</span> <span class="PercentText">82%</span>
+            </div>
+          </div>
+          <div class="skillSpecs">
+                  <span>Server Config. | Version Control | SFTP</span>
+                </div>
+              </div>
+        <div class="grid-row">
+          <div class="SkillBar">
+            <div id="Skill-Copy">
+              <span class="Skill-Area">Writing &amp; Editing</span> <span class="PercentText">91%</span>
+            </div>
+          </div>
+          <div class="skillSpecs">
+            <span>Creative | AP &amp; MLA Style | Research</span>
+          </div>
+        </div>
+        <div class="grid-row">
+          <div class="SkillBar">
+            <div id="Skill-Interpersonal">
+              <span class="Skill-Area">Interpersonal</span> <span class="PercentText">79%</span>
+            </div>
+          </div>
+          <div class="skillSpecs">
+            <span>Articulate | Ethical | Problem Solver | Weird A.F.</span>
+                </div>
+              </div>
+      </div>
+    </div>
+  </section> 
+  
+  <!-- # -->
+  
 </main>
 
 <!-- ************************************************** -->
