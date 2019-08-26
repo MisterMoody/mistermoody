59694848b2ee9933e54488ffe74cb20b0547b80f
MisterMoody <principal@mistermoody.com>
2019-08-26 10:48:23 -0400

    Add <main> Container => Remove from Individual Pages

--- inc/header.php
+++ inc/header.php
@@ -9,29 +9,26 @@
     <meta name="keywords" content="" />
     <!-- Browser Tab Icon -->
     <link rel="shortcut icon" href="img/me.jpg" type="img/m" class="rounded">    
-    <!-- CSS Styles -->   
-    <link rel="stylesheet" href="css/grid.css">
-    <!-- Custom Fonts -->       
-	  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bree+Serif|Raleway">
-    <!-- FontAwesome Fonts -->
+    <!-- FontAwesome ICON Fonts -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- CSS Font Stylesheet -->       
+	  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bree+Serif|Raleway">
+    <!-- Custom CSS Styles -->   
+    <link rel="stylesheet" href="css/grid.css">
   </head>
   <body>
-  <!-- ********************************* -->
-  <!-- ** // GRID CONTAINER 'start' **** -->
-  <!-- ********************************* -->
-    <article class="indexGrid">    
-      
-      <!--  // HEADER ( Nav / Logo ) *** -->
-      <header class="topnav" id="myTopnav">
-        <a href="index" class="active"><i class="fa fa-home"></i> Mister Moody</a>
-        <a href="about">About</a>
-        <a href="curriculums">Curriculums</a>
-        <a href="contact">Contact</a>
-        <a href="javascript:void(0);" class="icon" onclick="topNav()"><i class="fa fa-bars responsive"></i></a>
-      </header>
-      <!-- ***************************** -->   
-            
-      <!-- ***************************** -->
-      <!-- // CONTENT-BEGINS............ -->
-      <!-- ***************************** -->
+    <!-- ********************************* -->    
+    <!-- // HEADER + Global NAVIGATION *** -->
+    <header class="topnav" id="myTopnav">
+      <a href="index" class="active"><i class="fa fa-home"></i> Mister Moody</a>
+      <a href="about">About</a>
+      <a href="curriculums">Curriculums</a>
+      <!--<a href="resume">Resume</a>
+      <a href="webdesign">Web Design</a>
+      <a href="contact">Contact</a>-->
+      <a href="javascript:void(0);" class="icon" onclick="topNav()"><i class="fa fa-bars responsive"></i></a>
+    </header>
+    <!-- ********************************* -->
+    <!-- ** // Start of GRID CONTAINER *** -->
+    <main>
+
