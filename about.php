7ce5d78b3e1ebc479e18b8eeb4be375bf66652a1
MisterMoody <principal@mistermoody.com>
2019-08-25 20:16:05 -0400

    Update Content

--- about.php
+++ about.php
@@ -5,9 +5,235 @@ include ("inc/header.php"); //Contains 'metadata' and '<navigation> element'
 <!-- ************************************************** -->
 <!-- ******************** <<HOMEPAGE>>  *************** -->
 
-<main>
+<!-- //CONTENT-BEGINS..... -->
 
-</main>
+  <article class="resumeGrid">
+    <!--<section class="resume__author">
+      <div>
+        <div class="resume__title">Ray Moody</div>
+        <div class="res__sectionTitle">WebDevEduPreneur</div>
+      </div>
+      <div class="resume__contact">
+        <ul>
+          <li>&#40;502&#41; 558&#45;9240</li>
+          <li>principal&#64;mistermoody&#46;com</li>
+          <li>Louisville, Kentucky 40211</li>
+        </ul>
+      </div>
+    </section>-->
+    <!--  Background -->
+    <section class="resume__background">
+      <h2 class="res__sectionTitle">Background</h2>
+      <div>
+        <p>
+          I was born in Louisville, Kentucky and raised in a single-parent household. The relationship between my father and I was broken at age seven, and his abscence could best explain the behavioral and emotional rollercoaster that I would endure throughtout my youth. Environmental and financial woes caused my family to be uprooted frequently while being compelled to make due with scarce provisions. 
+        </p>
+        <p>
+          At age 11, I discovered a passion for playing basketball, and devoted all of my energy toward the sport. Like many of the kids that I grew up with, attending college was an elusive life experience attainable only through athletic scholarship. I developed well as a player competing in amateur athletics and was enrolled at an academically rigorous institution that allowed me to showcase my athletic talents while preparing me for a tertiary education. As a four year letterman and team captain, I led my school to a state tournament and had received my fair share of scholarship offers. That was until I blew out my knee and was forced to reevaluate my future as 100% of all offers were rescinded. Because I graduated in the top-percentile of my class, being accepted into a university was not much of a challenge. Paying the tuition, on the other hand, was and so I hedged a bet to mortgage my future for a university education! 
+        </p>
+        <p>
+          Basketball was always more than a game for me: it was an outlet for expression and a means to attain a middle-class lifestyle. Being an athlete allowed me to become a scholar. Being a scholar led me to study multiple disciplines at seven colleges and universities in three countries. Through education I was able to change the trajectory of my future and in the process I became an educator. 
+        </p>
+      </div>
+    </section>
+    <!-- Teaching Experience -->
+    <section class="resume__work">
+      <h2 class="res__sectionTitle">Teaching Experience</h2>
+      <div class="resume__Card">
+        <div class="res__work__Employment">
+          <div class="res__work__jobTitle">Substitute Teacher</div>
+          <div class="res__work__jobEmployer">Jefferson Co. Public Schools</div>
+          <div class="res__work__jobDate">1&#47;2018 &#45; Present</div>
+          <div class="res__work__jobLocation">Louisville, KY</div>
+        </div>
+        <div class="res__work__Responsibilities">
+          <ol>
+            <li>Responsible for maintaining class routines, keeping learners on task, creating a positive learning environment and encouraging cooperative learning techniques.</li>
+            <li>Completed professional development activities related to instructional strategies and accommodating the diverse needs of students as well as violence prevention, intervention and restraint.</li>
+            <li>Awarded 40 assignments at 16 schools; facilitated courses for more than 1,500 learners on subjects that include Computer Literacy, ESL, History and Math.</li>
+          </ol>
+        </div>
+      </div>
+      <div class="resume__Card">
+        <div class="res__work__Employment">
+          <div class="res__work__jobTitle">TEFL Specialist</div>
+          <div class="res__work__jobEmployer">Independent Contractor</div>
+          <div class="res__work__jobDate">8&#47;2003 &#45; 5&#47;2017</div>
+          <div class="res__work__jobLocation">Northeast Asia</div>
+        </div>
+        <div class="res__work__Responsibilities">
+          <ol>
+            <li>Nearly 10&#44;000 hours facilitating courses to learners of varying fluency</li>
+            <li>Awarded Foreign Expert Certificate by Peoples Republic of China to assist the foreign language departments at three state&#45;run institutions of education</li>
+            <li>Cooperated with private agencies and recruiters to scout ESL teacher candidates </li>
+            <li>Developed curricula materials and procurred authentic relia</li>
+          </ol>
+        </div>
+      </div>
+      <!--<details>
+        <summary>International Teaching Experience &nbsp;</summary>
+        <p>Below is a list of some of the educational institutions that I have collaborated with:</p>
+        <div class="trainingGrid">
+          <span>2016 - 2017 </span>
+          <span>TEFL Instructor <b>&#64;</b> Jiaxing College</span>
+          <span>2012 - 2013 </span>
+          <span>TEFL Instructor <b>&#64;</b> Zhejiang Middle School</span>
+          <span>2007 - 2009 </span>
+          <span>TEFL Trainer <b>&#64;</b> Oxford Seminars</span>
+          <span>2005 - 2006 </span>
+          <span>TEFL Instructor <b>&#64;</b> Joongdong High School</span>
+          <span>2004 - 2006 </span>
+          <span>TEFL Consultant <b>&#64;</b> US-Japan Edu. Exchange Program</span>
+          <span>2003 - 2004 </span>
+          <span>TEFL Instructor <b>&#64;</b> Sogang University</span>
+          <span>2000 - 2001 </span>
+          <span>Rec. Supervisor <b>&#64;</b> Cabbage Patch Settlement House</span>
+        </div> 
+      </details>-->
+    </section>
+    <!-- Training (Edu) -->
+    <section class="resume__education">
+      <h2 class="res__sectionTitle">Training</h2>
+      <div class="resume__Card">
+        <div class="res__work__Employment">
+          <div class="res__work__jobTitle">Web Development</div>
+          <div class="res__work__jobEmployer">&#60;Code&#62; Louisville</div>
+          <div class="res__work__jobDate">1&#47;2015 &#45; 9&#47;2015</div>
+          <div class="res__work__jobLocation">Louisville, KY</div>
+        </div>
+        <div class="res__work__Responsibilities">
+          <ol>
+            <li>Utilized <a href="https://teamtreehouse.com/mistermoody" target="_blank">online</a> curriculum to learn full-stack development best practices</li>
+            <li>Attended weekly meetups to strategize for how to tackle curriculum code challenges</li>
+            <li>Awarded Certificates of Completion for HTML, CSS, JavaScript, PHP and Android</li>
+          </ol>
+        </div>
+      </div>
+      <div class="resume__Card">
+        <div class="res__work__Employment">
+          <div class="res__work__jobTitle">Microsoft Office CERT</div>
+          <div class="res__work__jobEmployer">Microsoft</div>
+          <div class="res__work__jobDate">2&#47;2010</div>
+          <div class="res__work__jobLocation">Online</div>
+        </div>
+        <div class="res__work__Responsibilities">
+          <ol>
+            <li>Demonstrated proficiency using Office Suite</li>
+          </ol>
+        </div>
+      </div>
+      <div class="resume__Card">
+        <div class="res__work__Employment">
+          <div class="res__work__jobTitle">TEFL CERT</div>
+          <div class="res__work__jobEmployer">ITTT Inc.</div>
+          <div class="res__work__jobDate">5&#47;2007 &#45; 6&#47;2007</div>
+          <div class="res__work__jobLocation">Thailand</div>
+        </div>
+        <div class="res__work__Responsibilities">
+          <ol>
+            <li>Completed 100&#45;hour curriculum on topics that included classroom management, lesson planning and teaching English grammar to non-native speaker.</li>
+          </ol>
+        </div>
+      </div>
+      <div class="resume__Card">
+        <div class="res__work__Employment">
+          <div class="res__work__jobTitle">B.A. Political Science</div>
+          <div class="res__work__jobEmployer">the University of Louisville</div>
+          <div class="res__work__jobDate">1&#47;2000 &#45; 5&#47;2003</div>
+          <div class="res__work__jobLocation">Louisville, KY</div>
+        </div>
+        <div class="res__work__Responsibilities">
+          <ol>
+            <li>Chapter President: Pi Sigma Alpha</li>
+            <li>Student Assistant: Office of the President</li>
+            <li>Intern: Legislative Research Commission</li>
+            <li>Co&#45;Op: The Econ Village Group, LLC </li>
+            <li>Volunteer Tutor: Louisville Urban League</li>
+          </ol>
+        </div>
+      </div>
+      <!--<h5 class="resume__download">
+        <a class="download" href="moodyCV.doc" download="moodyCV.doc" title="Download Resume">Download Resume</a></h5>-->
+    </section>
+    <!-- Talents (Skills) + Goals -->
+    <section class="resume__skills">
+      <div class="subGrid">
+        <div>
+          <h2 class="res__sectionTitle">Talents</h2>
+          <p></p><!-- Need CSS for 'margin/padding SPACE' -->
+          <div>
+            <div class="grid-row">
+              <div class="SkillBar">
+                <div id="Skill-Design">
+                  <span class="Skill-Area">Design</span> <span class="PercentText">80%</span>
+                </div>
+              </div>
+              <div class="skillSpecs">
+                <span>HTML5 | CSS3 | IA/UI/UX | Accessibility</span>
+              </div>
+            </div>
+            <div class="grid-row">
+              <div class="SkillBar">
+                <div id="Skill-Develop">
+                  <span class="Skill-Area">Develop</span> <span class="PercentText">68%</span>
+                </div>
+              </div>
+              <div class="skillSpecs">
+                <span>PHP7 | Composer | JavaScript | npm</span>
+              </div>
+            </div>
+            <div class="grid-row">
+              <div class="SkillBar">
+                <div id="Skill-Deploy">
+                  <span class="Skill-Area">Deploy</span> <span class="PercentText">82%</span>
+                </div>
+              </div>
+              <div class="skillSpecs">
+                      <span>Server Config. | Version Control | SFTP</span>
+                    </div>
+                  </div>
+            <div class="grid-row">
+              <div class="SkillBar">
+                <div id="Skill-Copy">
+                  <span class="Skill-Area">Writing &amp; Editing</span> <span class="PercentText">91%</span>
+                </div>
+              </div>
+              <div class="skillSpecs">
+                <span>Creative | AP &amp; MLA Style | Research</span>
+              </div>
+            </div>
+            <div class="grid-row">
+            <div class="SkillBar">
+              <div id="Skill-Interpersonal">
+                <span class="Skill-Area">Interpersonal</span> <span class="PercentText">79%</span>
+              </div>
+            </div>
+            <div class="skillSpecs">
+              <span>Articulate | Ethical | Problem Solver | Weird A.F.</span>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div>
+          <h2 class="res__sectionTitle">Goals</h2>
+          <p>
+            I am currently engaged in self-fulfilling work teaching youth in my hometown after living abroad for most of my adult life. In desperate need of brushing up on my Mandarin and Spanish language skills. Coding like crazy! Thinking seriously about enrolling in an accredited graduate teaching program. Would like to open a neighborhood summer school that introduces individuals to software programming in an effort to enable folks with skills that they can use to jumpstart a web development career.
+          </p>
+        </div>
+      </div>
+    </section>
+    
+    <!--<section>
+      <p>
+        I put together a table of useful <a class="resourceText" href="webdesign" alt="Link to Resources" target="_blank" title="Online Resources">Online Resources</a> that help me build websites, which is a supplement to this <a class="resourceText" href="https://mistermoody.github.io/" alt="Link to Blog" target="_blank" title="Blog of Knowledge">DIY Guide</a> for creating a website.
+      </p>
+      <p>
+        Self-taught web implementation specialist able to proficiently design, develop and deploy a web application to specification. As a designer, I can adapt a low-fidelity prototype into a viable website using HTML, CSS, JavaScript, content management tools and digital media. As a developer, I utilize the PHP scripting language that incorporates object-oriented programming practices to integrate server-side functionality into a web application. Vital components of this process entails auscultating user-experience; amalgamating accessibility, performance requirements and other technical considerations; HTTP server installation; database configuration; and using the SSH protocol in conjunction with version control to provide secure remote access to mitigate maintenance concerns. To this end, I am currently exploring opportunities by which I can leverage my skillset to pursue a position in the tech industry.
+      </p>
+    </section>-->
+  </article>
+
+<!-- .....CONTENT-ENDS// --> 
 
 <!-- ************************************************** -->
 <!-- ************************************************** -->
