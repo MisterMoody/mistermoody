46067fab2e7ee3faab550336e0668f208e7bdc9e
MisterMoody <principal@mistermoody.com>
2019-08-25 20:45:50 -0400

    Update Index page

--- index.php
+++ index.php
@@ -7,64 +7,48 @@ include ("inc/header.php");
 <!-- ******************** <<HOMEPAGE>>  *************** -->
 
 
-<!-- //CONTENT-BEGINS..... -->
-<main>
-  <p style="height: 8vh;"></p>
-  <picture><a href="about.php"><img class="mainImage" src="img/MMgrey.jpg"></a></picture>
-  <!--<section>
-    <h1>Moody 2020</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  </section>
-  <hr>
-  <section>
-    <h2>4 Da Win</h2>
-    <ul>
-      <li>Ambition</li>
-      <li>Drive</li>
-      <li>Gumption</li>
-      <li>Redemption</li>
-    </ul>
-  </section>
-  <hr>
-  <section>
-    <h3>In an &#60;ALT&#62; universe...</h3>
-    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Tortor at risus viverra adipiscing at in tellus integer feugiat. Enim lobortis scelerisque fermentum dui faucibus. Suspendisse interdum consectetur libero id faucibus nisl tincidunt. Donec enim diam vulputate ut. Turpis massa sed elementum tempus. Vitae tortor condimentum lacinia quis vel. Quam nulla porttitor massa id. Tristique nulla aliquet enim tortor. Dictum at tempor commodo ullamcorper a. Tempor nec feugiat nisl pretium fusce id velit ut tortor. Aenean pharetra magna ac placerat vestibulum lectus mauris. Aenean vel elit scelerisque mauris pellentesque. Morbi quis commodo odio aenean sed. Varius morbi enim nunc faucibus a. Id volutpat lacus laoreet non.</p>
-    <p>Morbi non arcu risus quis. Fames ac turpis egestas maecenas pharetra convallis posuere. Eget est lorem ipsum dolor sit amet consectetur. Vestibulum morbi blandit cursus risus. Volutpat diam ut venenatis tellus in metus. Neque convallis a cras semper auctor neque vitae tempus. Pretium vulputate sapien nec sagittis. Diam in arcu cursus euismod quis viverra nibh cras.</p>
-    <p>Nisl condimentum id venenatis a condimentum vitae. Proin sed libero enim sed faucibus turpis. Non tellus orci ac auctor augue mauris augue. Elementum curabitur vitae nunc sed velit dignissim sodales. Elementum pulvinar etiam non quam lacus. Pretium aenean pharetra magna ac placerat. Commodo sed egestas egestas fringilla phasellus. Urna cursus eget nunc scelerisque viverra mauris in aliquam. Scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt. Aliquam id diam maecenas ultricies mi eget mauris. Augue ut lectus arcu bibendum at varius vel pharetra. Morbi tempus iaculis urna id. </p>
-  </section>
-  <hr>-->
-  <!-- #Contact -->
-  <p style="height: 8vh;"></p>
-  <section style="border: none; margin: 2em 0;">
-    <!--<h4>Get In Touch</h4>
-    <p class="readText">I am on the web. Check out my work on Github, follow me on Twitter, download my resume. Heck, give me a call or text me an email. Do it now!</p>-->
-    <ul class="social">
-      <li>
-        <a class="github" href="https://github.com/MisterMoody" target="_blank" title="Check Out My Work on Github">
-          <i class="fa fa-github-alt fa-2x" aria-hidden="true"></i></a>
-      </li>
-      <li>
-        <a class="twitter" href="https://twitter.com/Mister_Moody" target="_blank" title="Follow Me on Twitter">
-          <i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
-      </li>
-      <!--<li>
-        <a class="download" href="moodyCV.doc" download="moodyCV.doc" title="Download Resume">
-          <i class="fa fa-download fa-2x" aria-hidden="true"></i></a>
-      </li>-->
-      <li>
-        <a class="phone" href="tel:+15025589240" title="Call Me">
-          <i class="fa fa-mobile fa-2x" aria-hidden="true"></i></a>
-      </li>
-      <li>
-        <a class="email" href="mailto:raymoody10@gmail.com" title="Email Me">
-          <i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a>
-      </li>
-    </ul>
+<!-- ** // Start of CONTENT *** -->
+  <section class="intro">
+    <p>
+      <img class="imgL" src="img/workVanilla.jpg">Non-traditional educator with a plethora of experiences delivering language acquisition services for private and public institutions of education in China, Japan, South Korea and the United States.
+    </p>
+    <p>
+      Course facilitation practices that I promote amalgamates 21<sup>st</sup> Century Learning and Literacy Skills that encourage youth to be diligent learners in the classroom and on the internet. Core compenticies include critical processing of information, communicative etiquette and mutually beneficial collaboration with peers.
+    </p>
+    <h3 class="btn"><a href="about.php">Learn More</a></h3>
+    <p></p>
   </section>
   <!-- -->
-</main>
-<!-- .....CONTENT-ENDS// -->  
+  <section>
+    <!-- #Contact -->
+    <div>
+      <h3>Get In Touch</h3>
+      <!--<p class="readText">I am on the web. Check out my work on Github, follow me on Twitter, download my resume. Heck, give me a call or text me an email. Do it now!</p>-->
+      <ul class="social">
+        <li>
+          <a class="github" href="https://github.com/MisterMoody" target="_blank" title="Check Out My Work on Github">
+            <i class="fa fa-github-alt fa-2x" aria-hidden="true"></i></a>
+        </li>
+        <li>
+          <a class="twitter" href="https://twitter.com/Mister_Moody" target="_blank" title="Follow Me on Twitter">
+            <i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+        </li>
+        <!--<li>
+          <a class="download" href="moodyCV.doc" download="moodyCV.doc" title="Download Resume">
+            <i class="fa fa-download fa-2x" aria-hidden="true"></i></a>
+        </li>-->
+        <li>
+          <a class="phone" href="tel:+15025589240" title="Call Me">
+            <i class="fa fa-mobile fa-2x" aria-hidden="true"></i></a>
+        </li>
+        <li>
+          <a class="email" href="mailto:principal@mistermoody.com" title="Email Me">
+            <i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a>
+        </li>
+      </ul>
+      </div>
+  </section>
+<!-- ** // End of CONTENT *** --> 
 
 
 <!-- ************************************************** -->
