68fb70b1ce3ba76d0dc8d87ab5de4b2a9b6d0bd1
MisterMoody <principal@mistermoody.com>
2019-11-20 18:29:14 -0500

    Add Bootstrap 4 CDN <link> for 中文 CV

--- inc/header.php
+++ inc/header.php
@@ -16,6 +16,8 @@
     <link rel="shortcut icon" href="img/me.jpg" type="img/m" class="rounded">    
     <!-- FontAwesome ICON Fonts -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap 4 CDN for Chinese 'CV' Page -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- CSS Font Stylesheet -->       
 	  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bree+Serif|Raleway">
     <!-- Custom CSS Styles -->   
