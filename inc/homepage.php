c27ff3ce0e2eeaf1ba763202bf6238b36baa1569
MisterMoody <principal@mistermoody.com>
2019-11-20 18:31:48 -0500

    Install a <header> WITHOUT a 'Global <nav>'

---
+++ inc/homepage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0;">
+    <title>Mister Moody</title>
+    <meta name="author" content="Ray Moody">
+    <meta name="twitter:title" content="Mister_Moody">     <!-- Link to Twitter -->
+    <meta name="description" content="Official Site for Mister Moody">
+    <meta name="keywords" content="Mister Moody, TEFL, ESL, Web Developer, Copy Writer, Teacher, Louisville, Kentucky">
+    <!-- Absolute URL Reference [Req. to Add <links> to Subdirectory Files] -->
+    <!-- https://webmasters.stackexchange.com/questions/86450/htaccess-rewrite-url-leads-to-missing-css -->
+    <base href="http://localhost:8012/mistermoody/">
+    <!-- Browser Tab Icon -->
+    <link rel="shortcut icon" href="img/me.jpg" type="img/m" class="rounded">    
+    <!-- FontAwesome ICON Fonts -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- CSS Font Stylesheet -->       
+	  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bree+Serif|Raleway">
+    <!-- Custom CSS Styles -->   
+    <link rel="stylesheet" href="css/grid.css">
+  </head>
+  <body>
+    <!-- ********************************* -->
+    <!-- ** // Start of GRID CONTAINER *** -->
+    <main>
+
