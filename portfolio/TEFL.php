39ace550aba38107ec71ed3d46b5c94c07e54128
MisterMoody <principal@mistermoody.com>
2019-11-21 19:43:10 -0500

    Add Pages for 'Portfolio' Folder

---
+++ portfolio/TEFL.php
@@ -0,0 +1,24 @@
+<?php
+$pageTitle = "";
+include ("../inc/header.php");
+?>
+<!-- ******************************************************* -->
+<!-- ************* <<XXXXXXXXXXXXXXXXXXXXXX>>  ************* -->
+
+
+<!-- ** // Start of CONTENT *** -->
+  <section>
+    <h1>[ <b><em>TEFL Teaching</em></b> File ]</h1>
+    <h6>Nothing to see here. Move along!</h6>
+  </section>
+<!-- -->
+  <article class="">
+  </article>
+<!-- ** // End of CONTENT ***** --> 
+
+
+<!-- ************************************************** -->
+<!-- ************************************************** -->
+<?php
+include ("../inc/footer.php"); 
+?>
