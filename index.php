3be8ed92c23386b9739f7ef4ce43d0781d122d9f
MisterMoody <principal@mistermoody.com>
2019-11-21 19:21:03 -0500

    Update Homepage

--- index.php
+++ index.php
@@ -28,27 +28,25 @@ include ("inc/homepage.php");
       <a href="portfolio/">Portfolio</a> &nbsp;&nbsp; &#124; &nbsp;&nbsp; 
       <a href="contact">Contact</a>
     </h4>
-  </section>
-  <!-- #Contact + Social Media -->
-  <section>
-      <ul class="social">
-        <li>
-          <a class="github" href="https://github.com/MisterMoody" target="_blank" title="Check Out My Github Portfolio">
-            <i class="fa fa-github-alt fa-2x" aria-hidden="true"></i></a>
+    <!-- #Contact + Social Media -->
+    <ul class="social">
+      <li>
+        <a class="github" href="https://github.com/MisterMoody" target="_blank" title="Check Out My Github Portfolio">
+          <i class="fa fa-github-alt fa-2x" aria-hidden="true"></i></a>
         </li>
-        <li>
-          <a class="twitter" href="https://twitter.com/Mister_Moody" target="_blank" title="Follow Me on Twitter">
-            <i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+      <li>
+        <a class="twitter" href="https://twitter.com/Mister_Moody" target="_blank" title="Follow Me on Twitter">
+          <i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
         </li>
-        <li>
-          <a class="phone" href="tel:+15025589240" title="Call Me">
-            <i class="fa fa-mobile fa-2x" aria-hidden="true"></i></a>
+      <li>
+        <a class="phone" href="tel:+15025589240" title="Call Me">
+          <i class="fa fa-mobile fa-2x" aria-hidden="true"></i></a>
         </li>
-        <li>
-          <a class="email" href="mailto:principal@mistermoody.com" title="Email Me">
-            <i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a>
+      <li>
+        <a class="email" href="mailto:principal@mistermoody.com" title="Email Me">
+          <i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a>
         </li>
-      </ul>
+    </ul>
   </section>
 </article>
 <!-- ** // End of CONTENT ***** --> 
