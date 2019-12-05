7693f3d1e9f1254db2d3da641c96e9f41fbd75dc
MisterMoody <principal@mistermoody.com>
2019-12-05 14:09:22 -0500

    Add sub folder to portfolio/

---
+++ portfolio/languagearts/index.php
@@ -0,0 +1,32 @@
+<?php
+$pageTitle = "";
+include ("../../inc/header.php");
+?>
+<!-- ******************************************************* -->
+<!-- ************* <<XXXXXXXXXXXXXXXXXXXXXX>>  ************* -->
+
+
+<!-- ** // Start of CONTENT *** -->
+  <section style="height: 78vh;">
+    <h2>Language Arts</h2>
+    <article>
+      <p>
+        Items in this collection include ESL and ESP lesson plans that will demonstrate my ability to write concise objectives that explains an activity, the level of interaction and timing for each activity. Instructional strategies are highlighted to help the reader visualize how to apply such techniques.
+      </p>
+      <h3>Coming Soon</h3>
+      <!--<div class="tags">
+        <div>ESL</div>
+        <div>ESP</div>
+        <div>TEFL</div>
+        <div>Toolkit</div>
+      </div>-->
+    </article>
+  </section>
+<!-- ** // End of CONTENT ***** --> 
+
+
+<!-- ************************************************** -->
+<!-- ************************************************** -->
+<?php
+include ("../../inc/footer.php"); 
+?>
