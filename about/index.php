7fe919322edb512758273d8df403b5d07277080e
MisterMoody <principal@mistermoody.com>
2019-12-19 14:30:25 -0500

    Update Desktop Styles for Contact Form + Extend Length of FadIn Animation

--- about/index.php
+++ about/index.php
@@ -9,6 +9,16 @@ include ("../inc/header.php")
 <!-- ** // Start of CONTENT *** -->
   <section>
     
+    <article>
+      <p>
+        My career as an educator began when I was an undergraduate employed as a supervisor for an at-risk youth community center. Shortly after graduating university, I embarked on a sojourn abroad to teach English as a second language. 
+      </p>
+      <p>
+       What was supposed to have been a gap year turned into a life altering experience in which my life was immersed in foreign culture.  Being a pseudo-polyglot allowed me to empathize with language acquisition struggles learners encounter and I believe this helped me thrive in assisting learners from China, Japan, South Korea and the United States improve their language fluency. Some of the agencies that have enabled me to practice my craft include Sogang Uni., Oxford Seminars, Joongdong High School, YBM International Academy, Tongxiang Middle School, Jiaxing College and Jefferson County Public Schools (Kentucky). 
+      </p>
+      <h5>Check out my <a href="about/resume">resume</a> &nbsp;<i class="fa fa-angle-double-right"></i></h5>
+    </article>
+    
     <!-- FULL-SCREEN <image> GALLERY -->    
     <aside class="imageGrid">
       <!-- Row of 4 -->
@@ -25,17 +35,24 @@ include ("../inc/header.php")
       <img src="img/hk.jpg">
       <img src="img/yungn.jpg" style="height: 100%;">
       <img src="img/krwvisa.jpg" style="height: 100%;">
+      
+      <!-- newly added 
+      <img src="img/westerES.jpg" style="height: 100%;">
+      <img src="img/brandeisES.jpg" style="height: 100%;">
+      <img src="img/GH-Shyrock.jpg" style="height: 100%;">
+      <img src="img/coleridgeES.jpg" style="height: 100%;">
+      
+      <img src="img/ChienShieung.jpg" style="height: 100%;">
+      <img src="img/KingsTable.jpg" style="height: 100%;">
+      <img src="img/jpnPlug.jpg" style="height: 100%;">
+      <img src="img/krw.jpg" style="height: 100%;">
+      
+      <img src="img/startupWKND.jpg" style="height: 100%;">
+      <img src="img/tongxiang.jpg" style="height: 100%;">
+      <img src="img/ybm.jpg" style="height: 100%;">
+      <img src="img/withGov.Patton.jpg" style="height: 100%;">
+-->
     </aside>
-
-    <article>
-      <p>
-        My career as an educator began when I was an undergraduate employed as a supervisor for an at-risk youth community center. Shortly after graduating university, I embarked on a sojourn abroad to teach English as a second language. 
-      </p>
-      <p>
-       What was supposed to have been a gap year turned into a life altering experience in which my life was immersed in foreign culture.  Being a pseudo-polyglot allowed me to empathize with language acquisition struggles learners encounter and I believe this helped me thrive in assisting learners from China, Japan, South Korea and the United States improve their language fluency. Some of the agencies that have enabled me to practice my craft include Sogang Uni., Oxford Seminars, Joongdong High School, YBM International Academy, Tongxiang Middle School, Jiaxing College and Jefferson County Public Schools (Kentucky). 
-      </p>
-      <h5>Check out my <a href="about/resume">resume</a> &nbsp;<i class="fa fa-angle-double-right"></i></h5>
-    </article>
   </section>  
 <!-- ** // End of CONTENT ***** --> 
 
