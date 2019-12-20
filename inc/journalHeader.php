c6913badb76e5c21b12fa977388d448ba33ee856
MisterMoody <principal@mistermoody.com>
2019-12-19 23:57:52 -0500

    Instantiate 'journal/' Folder to Contain Journal-Articles

---
+++ inc/journalHeader.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0;">
+    <title>Mister Moody</title>
+    <meta name="author" content="Ray Moody">
+    <meta name="twitter:title" content="Mister_Moody">     <!-- Link to Twitter -->
+    <meta name="description" content="Journal de Mister Moody">
+    <meta name="keywords" content="Mister Moody, Web Design, Web Developer, Copy Writer, Teacher, Louisville, Kentucky">
+    <!-- Absolute URL Reference '.htaccess URL Rewrite -->
+    <!-- [Req. to Add <links> to Subdirectory Files] -->
+    <base href="http://localhost:8012/mistermoody/">
+    <!--  Use this <link> to *GO-LIVE*  <base href="https://mistermoody.com">--> 
+    <!-- Browser Tab Icon -->
+    <link rel="shortcut icon" href="img/me.jpg" type="img/m" class="rounded">    
+    <!-- FontAwesome ICON Fonts -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- CSS Font Stylesheet -->       
+	  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bree+Serif|Raleway">
+    <!-- Custom CSS Styles -->   
+    <link rel="stylesheet" href="css/journalStyles.css">
+  </head>
+  <body>
+    <!-- ********************************* -->    
+    <!-- // HEADER + Global NAVIGATION *** -->
+    <header class="topnav" id="myTopnav">
+      <a href="index" class="active"><i class="fa fa-home"></i> Mister Moody</a>
+      <a href="about">About</a>
+      <a href="portfolio">Portfolio</a>
+      <a href="contact">Contact</a>
+      <a href="javascript:void(0);" class="icon" onclick="topNav()"><i class="fa fa-bars responsive"></i></a>
+    </header>
+    <!-- ********************************* -->
+    <!-- ** // Start of GRID CONTAINER *** -->
+    <main>
+
