6e957378815dd1705e5f14cdba5f45d67de5ebb9
MisterMoody <principal@mistermoody.com>
2019-12-25 18:09:48 -0500

    Simple Style Edit

--- portfolio/index.php
+++ portfolio/index.php
@@ -9,22 +9,22 @@ include ("../inc/header.php");
   <article class="">
     <section>
       <p>Assembled herein you will find a collection of items which demonstrate my knowledge to develop web applications and facilitate TEFL instruction.</p>
-      <p style="text-align: center;"><i class="fa fa-warning" aria-hidden="true"></i> &nbsp; In-Progress: Return 1 January 2020</p>
+      <p style="text-align: center;"><i class="fa fa-warning" aria-hidden="true"></i> &nbsp; In-Progress: Return January 2020</p>
     </section>
   </article>
 
   <article class="cur__TopicGrid">
     <section class="cur__TopicStyle">
       <div class="language">
-        <h3><i class="fa fa-language" aria-hidden="true"></i> Language Arts</h3> 
+        <h3><i class="fa fa-language fa-2x" aria-hidden="true"></i> Language Arts</h3> 
       </div>
-      <p><a href="portfolio/languagearts">Enter Portal</a> &nbsp; <i class="fa fa-angle-double-right"></i></p>
+      <p><a href="portfolio/languagearts">Enter Portal</a></p>
     </section>
     <section class="cur__TopicStyle">
       <div class="code">
-        <h3><i class="fa fa-code" aria-hidden="true"></i> Web Implementations</h3> 
+        <h3><i class="fa fa-code fa-2x" aria-hidden="true"></i> Web Implementations</h3> 
       </div>
-      <p><a href="portfolio/webdesign">Enter Portal</a> &nbsp; <i class="fa fa-angle-double-right"></i></p>
+      <p><a href="portfolio/webdesign">Enter Portal</a></p>
     </section>
   </article>
 <!-- ******************************************************* -->
