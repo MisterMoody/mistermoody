2ccd6225be608b19651c972bec8652ae18c3a132
MisterMoody <principal@mistermoody.com>
2020-01-03 16:41:46 -0500

    Update About Content + Hide Image Gallery

--- about/index.php
+++ about/index.php
@@ -8,35 +8,48 @@ include ("../inc/header.php")
 
 <!-- ** // Start of CONTENT *** -->
   <section>
-    
+    <!-- // Introduction -->
     <article>
       <p>
+        I was born in Louisville, Kentucky and raised in a single-parent household. The relationship between my father and I had severed at age seven, and his absence could best explain the behavioral and emotional rollercoaster that I would endure throughout my youth. Environmental and financial woes frequently displaced my family, but we were able to cope with scarce provisions.
+      </p>
+      <p>
+        At age 11, I discovered a passion for playing basketball, and devoted all of my energy toward the sport. Like many of the kids that I grew up with, attending college was an elusive life experience attainable only through athletic scholarship. I developed well as a player competing in amateur athletics, which led to my enrollment at an academically rigorous institution that allowed me to showcase my athletic talents while preparing me for a tertiary education. As a senior and team captain, I led my school to a state tournament and had received my fair share of scholarship offers. That was until I blew out my knee: my injury scared aware every school that had recruited me, resulting in 100% of all offers being rescinded. Because I graduated in the top-percentile of my class, being accepted into a university was not much of a challenge. Paying the tuition, on the other hand, was and so I hedged a bet to mortgage my future for a university education!
+      </p>
+      <p>
+        While attending university, I became interested in economics and political science: I wanted to better understand the system functionality of American democracy. The opportunities to explore this field were ample while living the student life: I performed internships with local, state and federal government agencies, explored tort law at a prestigious university and provided assistance for a limited liability company with drafting an overseas private investment corporation application. As a university graduate, however, I was not as fortunate in procuring suitable employment related to my degree. Moreover, I had come to the stark reality that attending law school was an expense that I simply could not afford. Instead of settling for the first job to become available, I embarked on a sojourn abroad to teach English as a Second Language. I landed in South Korea three months after graduating university with the mindset that I would save up for law school. That didn't happen. Instead, I began to explore the world, having since travelled to nearly two dozen nations. I accomplished these feats, and many more, through my service to others which was teaching~
+      </p>
+      <p>
+        Basketball was always more than a game for me: it was an outlet for expression and a means to achieve the American dream. Being an athlete allowed me to become a scholar. Being a scholar led me to study multiple disciplines at seven colleges and universities in three countries. Through education I altered the trajectory of my future and in the process I became an educator.
+      </p>
+      <!--<p>
         My career as an educator began when I was an undergraduate employed as a supervisor for an at-risk youth community center. Shortly after graduating university, I embarked on a sojourn abroad to teach English as a second language. 
       </p>
       <p>
-       What was supposed to have been a gap year turned into a life altering experience in which my life was immersed in foreign culture.  Being a pseudo-polyglot allowed me to empathize with language acquisition struggles learners encounter and I believe this helped me thrive in assisting learners from China, Japan, South Korea and the United States improve their language fluency. Some of the agencies that have enabled me to practice my craft include Sogang Uni., Oxford Seminars, Joongdong High School, YBM International Academy, Tongxiang Middle School, Jiaxing College and Jefferson County Public Schools (Kentucky). 
-      </p>
-      <h5>Check out my <a href="about/resume">resume</a> &nbsp;<i class="fa fa-angle-double-right"></i></h5>
+        What was supposed to have been a gap year turned into a life altering experience in which my life was immersed in foreign culture.  Being a pseudo-polyglot allowed me to empathize with language acquisition struggles learners encounter and I believe this helped me thrive in assisting learners from China, Japan, South Korea and the United States improve their language fluency. Some of the agencies that have enabled me to practice my craft include Sogang Uni., Oxford Seminars, Joongdong High School, YBM International Academy, Tongxiang Middle School, Jiaxing College and Jefferson County Public Schools (Kentucky). 
+      </p>-->
+      <p>Along my journey, I have staggered, struggled and stumbled miserably, but I never stopped chasing the dream. </p>
+      <h5>Check out my <a href="about/resume">resume</a></h5>
     </article>
     
     <!-- FULL-SCREEN <image> GALLERY -->    
-    <aside class="imageGrid">
-      <!-- Row of 4 -->
+    <!--<aside class="imageGrid">
+       Row of 4 
       <img src="img/3yrjpn.jpg">
       <img src="img/cnID.jpg" style="height: 100%;">
       <img src="img/cnvisa.jpg" style="height: 100%;">
       <img src="img/jpnstamp.jpg" style="height: 100%;">
-      <!-- Dbl-Row of 2+1 -->
+       Dbl-Row of 2+1 
       <img src="img/jdhs.jpg">
       <img src="img/Fadeaway.jpg">
-      <!-- Row of 4 -->
+       Row of 4 
       <img src="img/cn09.jpg" style="height: 100%;">
       <img src="img/jpnyahata.jpg" style="height: 100%;">
       <img src="img/hk.jpg">
       <img src="img/yungn.jpg" style="height: 100%;">
       <img src="img/krwvisa.jpg" style="height: 100%;">
       
-      <!-- newly added 
+      
       <img src="img/westerES.jpg" style="height: 100%;">
       <img src="img/brandeisES.jpg" style="height: 100%;">
       <img src="img/GH-Shyrock.jpg" style="height: 100%;">
@@ -51,8 +64,8 @@ include ("../inc/header.php")
       <img src="img/tongxiang.jpg" style="height: 100%;">
       <img src="img/ybm.jpg" style="height: 100%;">
       <img src="img/withGov.Patton.jpg" style="height: 100%;">
--->
-    </aside>
+
+    </aside>-->
   </section>  
 <!-- ** // End of CONTENT ***** --> 
 
