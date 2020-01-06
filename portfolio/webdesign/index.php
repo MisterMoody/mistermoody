631946d1d0623033b6ce8484f6b332321d7e1622
MisterMoody <principal@mistermoody.com>
2020-01-05 19:58:22 -0500

    Remove Flickity.js Slider => Implement Row-Grid for Article List

--- portfolio/webdesign/index.php
+++ portfolio/webdesign/index.php
@@ -12,151 +12,153 @@ include ("../../inc/header.php");
     <article>
       <h3>Web Implementations</h3>
       <p>
-      This collection of articles demonstrate my proficiencies as a web developer to design, develop and deploy a web application. 
+      This collection of articles demonstrate my proficiencies to design, develop and deploy a website. 
       </p>
     </article>
   </section> 
-    <!-- -->
-  <section>
-    <article class="gallery js-flickity">
-			<div class="gallery-cell">
-        <h5 class="article__Item__Headline">Setting up a Workstation</h5>
+  <!-- -->
+  <article>
+    <h1></h1>
+    <section class="chapter__book">
+			<div class="chapter">
         <figure>
-          <img class="article__Item__Img" src="img/workstation.png">
-          <figcaption>A Simple Workstation</figcaption>
+          <img class="chapter__Img" src="img/workstation.png">
+          <figcaption class="">A Simple Workstation</figcaption>
         </figure>
-        <aside class="article__Text__Container">
-          <div class="article__Date_Tags">
+        <aside class="">
+          <h3 class="chapter__Headline">Workstation Set Up</h3>
+          <p class="chapter__Text">
+            Establishing a proper developer environment requires amalgamating technologies that will facilitate a process permitting developers to write, test and manage a web application. This can be achieved with a text-editor, a version controller and a local server. &nbsp;
+            <a href="journal/setup-workstation">Read More</a>
+          </p>
+          <div class="chapter__Date_Tag">
             <div>
               <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
             </div>
-            <div class="article__Tags">
+            <div class="chapter__Tags">
               <div>IDE</div>
               <div>Server</div>
               <div>VCS</div>
             </div>
           </div>
-          <p class="article__Item__Text">
-            Establishing a proper developer environment requires amalgamating technologies that will facilitate a process permitting developers to write, test and manage a web application. This can be achieved with a text-editor, a version controller and a local server. &nbsp;
-            <a href="journal/setup-workstation">Read More</a>
-          </p>
         </aside>
 			</div>
-			<div class="gallery-cell">
-        <h5 class="article__Item__Headline">Content Strategy</h5>
+      <div class="chapter">
         <figure>
-          <img class="responsive article__Item__Img" src="img/puzzle2.jpg">
-          <figcaption>Challenges Present Infinite Possibilities</figcaption>
+          <img class="chapter__Img" src="img/puzzle2.jpg">
+          <figcaption>There Will Be Challenges</figcaption>
         </figure>
-        <aside class="article__Text__Container">
-            <div class="article__Date_Tags">
-              <div>
-                <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
-              </div>
-              <div class="article__Tags">
-                <div>Design</div>
-                <div>Research</div>
-              </div>
-            </div>
-          <p class="article__Item__Text">
+        <aside class="">
+          <h3 class="chapter__Headline">Content Strategy</h3>
+          <p class="chapter__Text">
             Writing compelling copy and using media that properly articulates your message is of utmost importance before structuring a layout design for a website. Producing content that provides a meaningful user experience is the goal when executing a strategy. <!--&nbsp;
             <a href="portfolio/webdesign/">Read More</a>-->
           </p>
-          </aside>
-      </div>
-			<div class="gallery-cell">
-        <h5 class="article__Item__Headline">&#60;HTML&#62; Markup</h5>
-        <figure>
-          <img class="responsive article__Item__Img" src="img/html.png">
-            <figcaption>&#60;HTML&#62; Boilerplate</figcaption>
-        </figure>
-        <aside class="article__Text__Container">
-          <div class="article__Date_Tags">
+          <div class="chapter__Date_Tag">
             <div>
               <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
             </div>
-             <div class="article__Tags">
+            <div class="chapter__Tags">
+              <div>Design</div>
+              <div>Marketing</div>
+              <div>Research</div>
+            </div>
+          </div>
+        </aside>
+      </div>
+      <div class="chapter">
+        <figure>
+          <img class="chapter__Img" src="img/html.png">
+            <figcaption>&#60;HTML&#62; Boilerplate</figcaption>
+        </figure>
+        <aside class="">
+          <h3 class="chapter__Headline">&#60;HTML&#62; Markup</h3>
+          <p class="chapter__Text">
+            The Hyper Text Markup Language (HTML) is used to markup content using elements that specifies a purpose, determining how content will be displayed in a browser. An elements default functionality can be modified with attributes. &nbsp;
+            <a href="journal/semantic-markup">Read More</a>
+          </p>
+          <div class="chapter__Date_Tag">
+            <div>
+              <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
+            </div>
+             <div class="chapter__Tags">
                <div>HTML</div>
                <div>Markup</div>
              </div>
            </div>
-          <p class="article__Item__Text">
-            The Hyper Text Markup Language (HTML) is used to markup content using elements that specifies a purpose, determining how content will be displayed in a browser. Attributes can be applied within an element to modify its default functionality. &nbsp;
-            <a href="journal/semantic-markup">Read More</a>
-          </p>
         </aside>
       </div>
-			<div class="gallery-cell">
-        <h5 class="article__Item__Headline">Styling Content with CSS</h5>
+      <div class="chapter">
         <figure>
-          <img class="responsive article__Item__Img" src="img/designStyle.jpg" style="max-width: 100%;">
-            <figcaption>Web Design Word Cloud</figcaption>
+          <img class="chapter__Img" src="img/designStyle.jpg" style="max-width: 100%;">
+            <figcaption>So Many Colors</figcaption>
         </figure>
-        <aside class="article__Text__Container">
-          <div class="article__Date_Tags">
+        <aside class="">
+          <h3 class="chapter__Headline">Styling Markup</h3>
+          <p class="chapter__Text">
+              Cascading Style Sheets are used to define the aesthetic qualities desired from HTML markup. Selectors are used to target elements which invoke declarations of property-value pairings. There hundreds of properties available for designers to create magic! &nbsp; 
+            <a href="journal/styling-markup-with-css/">Read More</a>
+          </p>
+          <div class="chapter__Date_Tag">
             <div>
               <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
             </div>
-             <div class="article__Tags">
+             <div class="chapter__Tags">
                <div>CSS</div>
                <div>Design</div>
              </div>
            </div>
-          <p class="article__Item__Text">
-              Cascading Style Sheets are used to define the aesthetic qualities desired from HTML markup. Selectors are used to target elements which invoke declarations of property-value pairings. There hundreds of properties available for designers to create magic! &nbsp; 
-            <a href="journal/styling-markup-with-css/">Read More</a>
-          </p>
-        </aside>
+        </aside>        
       </div>
-			<div class="gallery-cell">
-        <h5 class="article__Item__Headline">Adding Interactivity</h5>
-        <div>
-          <img class="responsive article__Item__Img" src="img/chess.jpg">
-            <figcaption>The Game is Chess, not Checkers</figcaption>
-        </div>
-        <aside class="article__Text__Container">
-          <div class="article__Date_Tags">
+      <div class="chapter">
+        <figure>
+          <img class="chapter__Img" src="img/chess.jpg">
+            <figcaption>There Will Be Challenges</figcaption>
+        </figure>
+        <aside class="">
+          <h3 class="chapter__Headline">Adding Interactivity</h3>
+          <p class="chapter__Text">
+            JavaScript + PHP are programming languages that devs use to create dynamic elements and enhance user experience. For example, where HTML + CSS are used to markup and style a form, respectively, JS + PHP collects then processes form data. <!--&nbsp;
+            <a href="portfolio/webdesign/">Read More</a>-->
+          </p>
+          <div class="chapter__Date_Tag">
             <div>
               <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
             </div>
-             <div class="article__Tags">
+             <div class="chapter__Tags">
                <div>JS</div>
                <div>PHP</div>
                <div>Development</div>
              </div>
            </div>
-          <p class="article__Item__Text">
-            JavaScript + PHP are programming languages that devs use to create dynamic elements and enhance user experience. For example, where HTML + CSS are used to markup and style a form, respectively, JS + PHP collects then processes form data. <!--&nbsp;
-            <a href="portfolio/webdesign/">Read More</a>-->
-          </p>
-        </aside>
+        </aside>        
       </div>
-			<div class="gallery-cell">
-        <h5 class="article__Item__Headline">Resources</h5>
+      <div class="chapter">
         <figure>
-          <img class="responsive article__Item__Img" src="img/gold.jpg">
-            <figcaption>Worth its Weight in Gold!</figcaption>
+          <img class="chapter__Img" src="img/gold.jpg">
+            <figcaption>Need Help?</figcaption>
         </figure>
-        <aside class="article__Text__Container">
-          <div class="article__Date_Tags">
+        <aside class="">
+          <h3 class="chapter__Headline">Resources</h3>
+          <!-- Build a Glossary for SYNTAX -->
+          <p class="chapter__Text">
+              Boilerplates. Placeholders. Formatters. Forms. Infographics. Font-Scale. Typography. Color Swatches + Palettes. Contrast Checker. Image Optimizer. Performance Testers. Icons. Accessibility (A11Y). Compressors. Validators. Tutorials. UX Inspiration. ASCII. &nbsp;
+            <a href="portfolio/webdesign/resources">Read On</a>
+          </p>
+          <div class="chapter__Date_Tag">
             <div>
               <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
             </div>
-             <div class="article__Tags">
+             <div class="chapter__Tags">
                <div>Front-end</div>
                <div>Back-end</div>
                <div>DevOps</div>
              </div>
            </div>
-          <!-- Build a Glossary for SYNTAX -->
-          <p class="article__Item__Text">
-              Boilerplates. Placeholders. Syntax Formatters. Forms. Infographics. Font-Scale. Typography. Color Swatches + Palettes. Contrast Checker. Image Optimizer. Performance Testers. Icons. Accessibility (A11Y). Compressors. Validators. Tutorials. UX Inspiration. ASCII. &nbsp;
-            <a href="portfolio/webdesign/resources">Read On</a>
-          </p>
-        </aside>
+        </aside>        
       </div>
-		</article>
-  </section>
+    </section>
+  </article>
 <!-- .....CONTENT-ENDS// --> 
 
 <!-- ************************************************** -->
