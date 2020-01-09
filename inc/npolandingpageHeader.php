f84fcccc6ec22bee98b8ad4a83e9da65885d61ad
MisterMoody <principal@mistermoody.com>
2020-01-08 22:46:09 -0500

    Add 'projects/' Folder for 'webdesign/' Activities: 1st Project ~ 'Lovies House LANDING PAGE'

---
+++ inc/npolandingpageHeader.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0;">
+    <title>Lovie's House Landing Page</title>
+    <meta name="author" content="Ray Moody">
+    <meta name="facebook:title" content="Mister_Moody">     <!-- Link to Twitter -->
+    <meta name="description" content="Lovie's House Landing Page">
+    <meta name="subject" content="Capital Campaign Landing Page for Lovie's House">
+    <meta name="keywords" content="Mister Moody, TEFL, ESL, Web Developer, Copy Writer, Teacher, Louisville, Kentucky">
+    <!-- Absolute URL Reference '.htaccess URL Rewrite -->
+    <!-- [Req. to Add <links> to Subdirectory Files] -->
+    <base href="http://localhost:8012/mistermoody/">
+    <!--  Use this <link> to *GO-LIVE*  <base href="https://mistermoody.com">--> 
+    <!--  Facebook 'Open-Graph'
+    <meta property="fb:app_id" content="123456789">
+    <meta property="og:url" content="https://example.com/page.html">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Content Title">
+    <meta property="og:image" content="https://example.com/image.jpg">
+    <meta property="og:image:alt" content="A description of what is in the image (not a caption)">
+    <meta property="og:description" content="Description Here">
+    <meta property="og:site_name" content="Site Name">
+    <meta property="og:locale" content="en_US">
+    <meta property="article:author" content="">
+
+    <meta property="op:markup_version" content="v1.0">
+    ::The URL of the web version of your article 
+    <link rel="canonical" href="https://example.com/article.html">
+    -->
+    <!-- Browser Tab Icon -->
+    <link rel="icon" sizes="192x192" href="img/projectImages/logo.jpg" type="img/m" class="rounded">
+    <link rel="apple-touch-icon" href="mg/projectImages/logo.jpg" type="img/m" class="rounded">  
+    <!-- FontAwesome ICON Fonts -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- CSS Font Stylesheet -->       
+	  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bree+Serif|Raleway">
+    <!-- Custom CSS Styles -->   
+    <link rel="stylesheet" href="css/npolandingpage.css">
+  </head>
+  <body>
+    <!-- ********************************* -->
+    <!-- ** // Start of GRID CONTAINER *** -->
+    <main>
+
---
+++ webdesign/projects/npo-landing-page.php
@@ -0,0 +1,25 @@
+<?php
+$pageTitle = "Lovie's House Landing Page";
+include ("../../inc/npolandingpageHeader.php");
+?>
+<!-- ******************************************************* -->
+<!-- ************* <<XXXXXXXXXXXXXXXXXXXXXX>>  ************* -->
+
+
+<!-- ** // Start of CONTENT *** -->
+  <section class="hero">
+    <article class="heroImage">
+      <picture>
+        <img src="img/projectImages/logo2.jpg">
+      </picture>
+    </article>
+    <article class="content">
+      <h1>Lovie's House Incorporated</h1>
+      <p>Providing disenfranchised young men with nurturing opportunities that facilitate their well-being while encouraging them to become productive members of our community.</p>
+    </article>
+  </section>
+<!-- ** // End of CONTENT ***** --> 
+
+
+<!-- ************************************************** -->
+<!-- ************************************************** -->
