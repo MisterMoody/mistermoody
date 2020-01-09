170053461a666f4499d8ff3da32859b91d3929ab
MisterMoody <principal@mistermoody.com>
2020-01-09 16:12:41 -0500

    Update NPO Landing Page => Good Start

--- webdesign/projects/npo-landing-page.php
+++ webdesign/projects/npo-landing-page.php
@@ -18,30 +18,162 @@ include ("../../inc/npolandingpageHeader.php");
       <p>Providing disenfranchised young men with nurturing opportunities that facilitate their well-being while encouraging them to become productive members of our community.</p>
     </article>
   </section>
+<br>
 <!-- -->
+<section class="bButlerIntro">
+  <article class="blair"></article>
+  <article class="butler">
+    <p><b>Blair Butler</b> is a retired Jefferson County Public Schools Educator and Assistant Principal with more than 25 years experience developing the minds of youth in Louisville, Kentucky. He is a CommUNITY Champion on a mission to create pathways to success programs for young men transitioning out of foster care.</p>
+  </article>
+  <!--<video controls>
+    <source src="" type="video/mp4">
+    <source src="" type="video/webm">
+  </video>
+    <iframe width="500" height="320" src="https://www.wlky.com/article/community-champion-retired-jcps-teacher-to-launch-program-aimed-at-transitioning-foster-kids-into-adulthood/30300036">
+    </iframe>-->
+</section>
+<br>
+<!-- // Important Facts -->
 <section class="infoRow">
   <article class="wrapper">
-    <h2 class="infoRow__title">SOME TITLE</h2>
+    <h2 class="info__title">Troubling Facts</h2>
     <p class="infoRow__intro">
-      Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas faucibus mollis interdum. Curabitur blandit tempus porttitor.
+      Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas faucibus mollis interdum. Curabitur porttitor.
     </p>
-    <div class="box__grid">
-      <article class="box">
-        <a class="box__content" href="#">
-        <i class="fa fa-lightbulb-o fa-3x"></i>
-          <h3 class="box__title">Creative Idea</h3>
-          <p>Duis neque leo cursus ac libero ac imperdiet bibendum nisl mauris neque diam vulputate nem.</p>
-        <span class="box__more">Read more <i class="fa fa-arrow-right"></i></span></a>
+    <div class="info__grid">
+      <article class="infoBox">
+        <div class="info__content" href="/">
+          <i class="fa fa-graduation-cap fa-3x"></i>
+          <h3 class="info__title">Education</h3>
+          <p>Only 3&#37; of foster care youth will earn a college degree once they exit.</p>
+        </div>
       </article>
-      <article class="box"><a class="box__content" href="#"><i class="fa fa-code fa-3x"></i>
-          <h3 class="box__title">Clean Code</h3>
-          <p>Duis neque leo cursus ac libero ac imperdiet bibendum nisl mauris neque diam vulputate nem.</p><span class="box__more">Read more <i class="fa fa-arrow-right"></i></span></a></article>
-      <article class="box"><a class="box__content" href="#"><i class="fa fa-mobile fa-3x"></i>
-          <h3 class="box__title">Mobile First</h3>
-          <p>Duis neque leo cursus ac libero ac imperdiet bibendum nisl mauris neque diam vulputate nem.</p><span class="box__more">Read more <i class="fa fa-arrow-right"></i></span></a></article>
+      <article class="infoBox">
+        <div class="info__content">
+          <i class="fa fa-bed fa-3x"></i>
+          <h3 class="info__title">Housing</h3>
+          <p>40&#37; of foster care youth  have trouble finding affordable housing.</p>
+        </div>
+      </article>
+      <article class="infoBox">
+        <div class="info__content" href="/">
+          <i class="fa fa-balance-scale fa-3x"></i>
+          <h3 class="info__title">Legal</h3>
+          <p>60&#37; of foster care youth have been convisted of a crime once they exit.</p>
+        </div>
+      </article>
     </div>
   </article>
 </section>
+<br>
+<!-- Vision Statement -->
+<section>
+  <article class="content">
+    <h2>Vision Statement</h2>
+      <p class="specialMSG"><i>Our mission is to break the cycle of poverty for all young men.</i></p>
+  </article>
+  <article>
+  <picture>
+    <img src="img/projectImages/visionStatement.jpg">
+  </picture>
+  </article>
+</section>
+<br>
+<!-- // Vision Image Grid-->
+<section>
+  <article class="visionImageGrid">
+    <img src="img/projectImages/firstFloor.jpg">
+    <img src="img/projectImages/classroom.jpg">
+    <img src="img/projectImages/fitnessCenter.jpg">
+    <img src="img/projectImages/counselingSuites.jpg">
+    <img src="img/projectImages/commercialKitchen.jpg">
+    <img src="img/projectImages/adminOffices.jpg">
+    <img src="img/projectImages/atrium.jpg">
+    <img src="img/projectImages/secondFloor.jpg">
+  </article>
+</section>
+<br>
+<!-- MISC -->
+<section class="infoRow">
+  <article class="wrapper">
+    <h2 class="info__title">Misc</h2>
+    <p class="infoRow__intro">
+      Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas faucibus mollis interdum. Curabitur porttitor.
+    </p>
+    <div class="info__grid">
+      <article class="infoBox">
+        <div class="info__content">
+          <i class="fa fa-lightbulb-o fa-3x"></i>
+          <h3 class="info__title">Lorem Ipsum</h3>
+          <p>Duis neque leo cursus ac libero ac imperdiet bendum nisl mauris neque.</p>
+        </div>
+      </article>
+      <article class="infoBox">
+        <div class="info__content" href="/">
+          <i class="fa fa-chain-broken fa-3x"></i>
+          <h3 class="info__title">Lorem Ipsum</h3>
+          <p>Duis neque leo cursus ac libero ac imperdiet bendum nisl mauris neque.</p>
+        </div>
+      </article>
+      <article class="infoBox">
+        <div class="info__content" href="/">
+          <i class="fa fa-heartbeat fa-3x"></i>
+          <h3 class="info__title">Lorem Ipsum</h3>
+          <p>Duis neque leo cursus ac libero ac imperdiet bendum nisl mauris neque.</p>
+        </div>
+      </article>
+      <article class="infoBox">
+        <div class="info__content" href="/">
+          <i class="fa fa-facebook-official fa-3x"></i>
+          <h3 class="info__title">Lorem Ipsum</h3>
+          <p>Duis neque leo cursus ac libero ac imperdiet bendum nisl mauris neque.</p>
+        </div>
+      </article>
+      <article class="infoBox">
+        <div class="info__content" href="/">
+          <i class="fa fa-road fa-3x"></i>
+          <h3 class="info__title">Lorem Ipsum</h3>
+          <p>Duis neque leo cursus ac libero ac imperdiet bendum nisl mauris neque.</p>
+        </div>
+      </article>
+      <article class="infoBox">
+        <div class="info__content" href="/">
+          <i class="fa fa-car fa-3x"></i>
+          <h3 class="info__title">Lorem Ipsum</h3>
+          <p>Duis neque leo cursus ac libero ac imperdiet bendum nisl mauris neque.</p>
+        </div>
+      </article>
+      <article class="infoBox">
+        <div class="info__content" href="/">
+          <i class="fa fa-pie-chart fa-3x"></i>
+          <h3 class="info__title">Lorem Ipsum</h3>
+          <p>Duis neque leo cursus ac libero ac imperdiet bendum nisl mauris neque.</p>
+        </div>
+      </article>
+      <article class="infoBox">
+        <div class="info__content" href="/">
+          <i class="fa fa-users fa-3x"></i>
+          <h3 class="info__title">Lorem Ipsum</h3>
+          <p>Duis neque leo cursus ac libero ac imperdiet bendum nisl mauris neque.</p>
+        </div>
+      </article>
+      <article class="infoBox">
+        <div class="info__content" href="/">
+          <i class="fa fa-comments fa-3x"></i>
+          <h3 class="info__title">Lorem Ipsum</h3>
+          <p>Duis neque leo cursus ac libero ac imperdiet bendum nisl mauris neque.</p>
+        </div>
+      </article>
+    </div>
+  </article>
+</section>
+<!-- -->
+<section>
+      <picture>
+        <img src="img/projectImages/logo.jpg">
+      </picture></section>
+<!-- -->
+<footer>2020 Lovie's House Inc.</footer>
 <!-- ** // End of CONTENT ***** --> 
 
 
