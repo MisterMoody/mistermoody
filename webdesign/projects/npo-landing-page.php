879796e5f7d3db47a5941c29669642efcfd1bd43
MisterMoody <principal@mistermoody.com>
2020-01-08 23:30:56 -0500

    Add Content + Implement Flex/Grid Containers

--- webdesign/projects/npo-landing-page.php
+++ webdesign/projects/npo-landing-page.php
@@ -18,6 +18,30 @@ include ("../../inc/npolandingpageHeader.php");
       <p>Providing disenfranchised young men with nurturing opportunities that facilitate their well-being while encouraging them to become productive members of our community.</p>
     </article>
   </section>
+<!-- -->
+<section class="infoRow">
+  <article class="wrapper">
+    <h2 class="infoRow__title">SOME TITLE</h2>
+    <p class="infoRow__intro">
+      Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas faucibus mollis interdum. Curabitur blandit tempus porttitor.
+    </p>
+    <div class="box__grid">
+      <article class="box">
+        <a class="box__content" href="#">
+        <i class="fa fa-lightbulb-o fa-3x"></i>
+          <h3 class="box__title">Creative Idea</h3>
+          <p>Duis neque leo cursus ac libero ac imperdiet bibendum nisl mauris neque diam vulputate nem.</p>
+        <span class="box__more">Read more <i class="fa fa-arrow-right"></i></span></a>
+      </article>
+      <article class="box"><a class="box__content" href="#"><i class="fa fa-code fa-3x"></i>
+          <h3 class="box__title">Clean Code</h3>
+          <p>Duis neque leo cursus ac libero ac imperdiet bibendum nisl mauris neque diam vulputate nem.</p><span class="box__more">Read more <i class="fa fa-arrow-right"></i></span></a></article>
+      <article class="box"><a class="box__content" href="#"><i class="fa fa-mobile fa-3x"></i>
+          <h3 class="box__title">Mobile First</h3>
+          <p>Duis neque leo cursus ac libero ac imperdiet bibendum nisl mauris neque diam vulputate nem.</p><span class="box__more">Read more <i class="fa fa-arrow-right"></i></span></a></article>
+    </div>
+  </article>
+</section>
 <!-- ** // End of CONTENT ***** --> 
 
 
