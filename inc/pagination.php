63741902e717ef7e967302ccbbb32bc9fa7c4cf3
MisterMoody <principal@mistermoody.com>
2020-01-20 15:21:25 -0500

    Add Link for New Page

--- inc/pagination.php
+++ inc/pagination.php
@@ -3,7 +3,8 @@
     <li class="<?php echo ($page == "Workstation Setup" ? "active" : "")?>"><a href="webdesign/setup-workstation">1</a></li>
     <li class="<?php echo ($page == "Semantic Markup" ? "active" : "")?>"><a href="webdesign/semantic-markup">2</a></li>
     <li class="<?php echo ($page == "Styling Markup" ? "active" : "")?>"><a href="webdesign/styling-markup-with-css">3</a></li>
-    <li class="<?php echo ($page == "Git Control" ? "active" : "")?>"><a href="webdesign/git-control">4</a></li>
-    <li class="<?php echo ($page == "Resources" ? "active" : "")?>"><a href="webdesign/resources">5</a></li>
+    <li class="<?php echo ($page == "Optimization" ? "active" : "")?>"><a href="webdesign/optimization">4</a></li>
+    <li class="<?php echo ($page == "Git Control" ? "active" : "")?>"><a href="webdesign/git-control">5</a></li>
+    <li class="<?php echo ($page == "Resources" ? "active" : "")?>"><a href="webdesign/resources">6</a></li>
   </ul>
 </section>
