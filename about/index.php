2a246caf7533fc20d5e0644be1430badd6c19b04
MisterMoody <principal@mistermoody.com>
2020-01-22 17:07:57 -0500

    Update About Page

--- about/index.php
+++ about/index.php
@@ -11,11 +11,11 @@ include ("../inc/header.php")
   <h1 style="height: 28vh; margin: 5em auto">Apologies for the inconvenince. If you would like to view this web page, use a mobile device. Thank you.</h1>
 </section>
   <section class="hideDesktop">
-    <!-- Profile Card + Call-to-Action (Broken)-->
     <article>
+      <!-- Profile Card -->
       <aside class="profile__card--Grid">
         <div class="profile__card">
-        <div class="profile__info">
+          <div class="profile__info">
           <div class="rings">
             <div class="outer__ring"></div>
             <div class="inner__ring"></div>
@@ -37,47 +37,24 @@ include ("../inc/header.php")
             </div>
           </div>
         </div>
-      </div>
+        </div>
         <p class="profile__card--Paragraph">
           I am a substitute teacher with <a href="http://jefferson.kyschools.us" target="_blank">J.C.P.S.</a> (KY) serving the needs of students kindergarten through 12th grade. Since <code>January 2018</code>, I have been awarded <code>97</code> assignments at <code>41</code> schools being responsible for facilitating lesssons to more than <code>2,700</code> students.
         </p>
       </aside>
       <h2 style="font-variant: small-caps;">I make learning fun!</h2>
+      <!-- Social Proof: Image Gallery -->
       <aside class="imageGrid">
         <img src="img/clES2.jpg" style="height: 100%;">
         <img src="img/cleSTEMLab.png">
         <img src="img/clES1.jpg" style="height: 100%;">
+      </aside> 
+      <!-- Call-to-Action BTNs -->
+      <aside class="temp__btn-hold">
+        <button class="cta"><a href="contact">Need a Tutor?</a></button>
+        <button class="cta"><a href="about/resume">Check out my Resume</a></button> 
       </aside>
-      <div class="temp__btn-hold">
-      <button class="cta"><a href="contact">Need a Tutor?</a></button>
-      <button class="cta"><a href="about/resume">Check out my Resume</a></button> 
-      </div>
-    </article>
-    <!-- Career Experience -->       
-    <!--<article class="about__Intro">
-      <p class="about__Intro-Paragraph">
-        <img class="about__Intro-Image" src="img/me.jpg">My career as an educator began while studying political science as an undergraduate and simultaneously employed as a supervisor for an at-risk youth community center: I was 19 years old. Shortly after graduation, I embarked on a sojourn abroad to teach English as a second language. What was supposed to have been a gap year before enrolling in law school turned into a life altering experience that made me an educator. 
-      </p>
-    </article>-->
-    <!-- -->
-    <!-- 
-    Highlight 'Web Dev' Aspirations
-    <p><img src="img/startupWKND.jpg"></p>    // StartupWKND 
-
-    *<link> to Personal Projects as points are expressed
-    Mission Statement: Express Values & Goals + How I Can Help
-       [ Infuse 'Personality' to 'Forge a Connection'
-                                 'Inspire Trust'  ]
-    Credentials
-       [ Certifications + Work Photos/Social Proof + Testimonials ]
-    Call-to-Action 
-       [ Invitation to 'Follow' on Twitter 
-                       'Subscribe' to Newsletter
-                       'Donate' to Cause   ]
-
-    -->    
-    <!-- Social Proof: Image Gallery -->
-    <!--<article>
+      <!--
       <aside class="imageGrid">
         <img src="img/3yrjpn.jpg">
         <img src="img/yungn.jpg" style="height: 100%;">
@@ -93,37 +70,43 @@ include ("../inc/header.php")
         <img src="img/jpnyahata.jpg" style="height: 100%;">        
         <img src="img/coleridgeES.jpg">
         <img src="img/jdhs.jpg" style="height: 100%;">
-      </aside>
-    </article>-->
-    <!-- Career Experience -->
-    <!--<article>
+      </aside>-->
+    <!-- 
+    Highlight 'Web Dev' Aspirations
+    <p><img src="img/startupWKND.jpg"></p>    // StartupWKND 
+
+    *<link> to Personal Projects as points are expressed
+    Mission Statement: Express Values & Goals + How I Can Help
+       [ Infuse 'Personality' to 'Forge a Connection'
+                                 'Inspire Trust'  ]
+    Credentials
+       [ Certifications + Work Photos/Social Proof + Testimonials ]
+
+    -->
+      <!-- Career Experience --> 
       <p class="about__Intro-Paragraph">
-        Life as an emigrant was challenging, yet as a life-learner and pseudo-polyglot, I empathize well with language acquisition struggles learners encounter. My affinity with foreign culture and language began in my youth watching Chinese kung fu movies. This sparked a curiosity that would lead me to study French, Spanish, Hangul, Nihongo and Putonghua.
+        My career as an educator began while studying political science as an undergraduate student employed as a supervisor for an at-risk youth community center: I was 19 years old.  
       </p>
+      <figure><img class="mainImage" src="img/withGov.Patton.jpg"></figure>
+      <p class="about__Intro-Paragraph">
+        Shortly after graduation, I embarked on a sojourn abroad to teach English as a second language. What was supposed to have been a gap year before enrolling in law school turned into a life altering experience that made me an educator.
+      </p>
+      <figure><img class="mainImage" src="img/knowledge.jpg"></figure>
+      <p class="about__Intro-Paragraph">
+        Life as an emigrant was challenging, yet as a life-learner and pseudo-polyglot, I empathize well with language acquisition struggles learners encounter. My affinity with foreign culture and language dates back to my youth: this curiosity would later encourage me to study Francais, Espanol, Hangul, Nihongo and Putonghua.
+      </p>
+      <figure><img class="mainImage" src="img/tailorVanilla.jpg"></figure>
       <p class="about__Intro-Paragraph">
         That same inquisitiveness led me to travel abroad, beyond the wilderness of imagination and into the depths of a holistic cultural immersion from which I emerged an ardent spirit of humanity. Throughout China, Japan and South Korea, I thrived in supporting learners improve their language fluency. Some of the agencies that have enabled me to practice my craft include Sogang University, Oxford Seminars, Joongdong High School, YBM International Academy, Chien-Shiung Institute of Technology, Tongxiang Middle School, Shanghai Ocean University and Jiaxing College.
       </p>
-    </article>-->
-    <!-- <Link> to Resume -->
-    <!--<article>
-      <button class="cta"><a href="about/resume">Check out my Resume</a></button> 
-    </article>--> 
-    <!-- Long-Version BIO -->
-    <!--<article>
-      <p>
-        I was born in Louisville, Kentucky and raised in a single-parent household. The relationship between my father and I had severed at age seven, and his absence could best explain the behavioral and emotional rollercoaster that I would endure throughout my youth. Environmental and financial woes frequently displaced my family, but we were able to cope with scarce provisions.
+      <br>
+      <figure><img class="mainImage" src="img/startupWKND.jpg"></figure>      
+      <p class="about__Intro-Paragraph">
+        I became interested in web technologies pre-2020, first learning how to markup, style and make a website interactive using HTML, CSS, JavaScript and PHP. Participating in a pseudo <a href="https://codelouisville.org" target="_blank">bootcamp</a> taught me to 'fail fast' and embrace the craft as a journey while helping organize a major local <a href="https://startupweekendlouisville.com/" target="_blank">event</a> has motivated me to do more for this ecosystem. Big plans for a cross-over career: keep an eye out for the completion of my first <a href="webdesign/">e-book</a>.
       </p>
-      <p>
-        At age 11, I discovered a passion for playing basketball, and devoted all of my energy toward the sport. Like many of the kids that I grew up with, attending college was an elusive life experience attainable only through athletic scholarship. I developed well as a player competing in amateur athletics, which led to my enrollment at an academically rigorous institution that allowed me to showcase my athletic talents while preparing me for a tertiary education. As a senior and team captain, I led my school to a state tournament and had received my fair share of scholarship offers. That was until I blew out my knee: my injury scared aware every school that had recruited me, resulting in 100% of all offers being rescinded. Because I graduated in the top-percentile of my class, being accepted into a university was not much of a challenge. Paying the tuition, on the other hand, was and so I hedged a bet to mortgage my future for a university education!
-      </p>
-      <p>
-        While attending university, I became interested in economics and political science: I wanted to better understand the system functionality of American democracy. The opportunities to explore this field were ample while living the student life: I performed internships with local, state and federal government agencies, explored tort law at a prestigious university and provided assistance for a limited liability company with drafting an overseas private investment corporation application. As a university graduate, however, I was not as fortunate in procuring suitable employment related to my degree. Moreover, I had come to the stark reality that attending law school was an expense that I simply could not afford. Instead of settling for the first job to become available, I embarked on a sojourn abroad to teach English as a Second Language. I landed in South Korea three months after graduating university with the mindset that I would save up for law school. That didn't happen. Instead, I began to explore the world, having since travelled to nearly two dozen nations. I accomplished these feats, and many more, through my service to others which was teaching~
-      </p>
-      <p>
-        Basketball was always more than a game for me: it was an outlet for expression and a means to achieve the American dream. Being an athlete allowed me to become a scholar. Being a scholar led me to study multiple disciplines at seven colleges and universities in three countries. Through education I altered the trajectory of my future and in the process I became an educator.
-      </p>
-    </article>-->
-    
+      <!-- Link to Resume -->
+      <button class="cta"><a href="about/resume">Check out my Resume</a></button>
+    </article>
     <!-- FULL-SCREEN <image> GALLERY -->    
     <!--<aside class="imageGrid">
        Row of 4 
