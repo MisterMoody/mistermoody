14fc6f2c6f2f8b25634a6a8a4491362238c1a61c
MisterMoody <principal@mistermoody.com>
2020-01-26 11:55:54 -0500

    Instantiate 'Zhongwen Wenhua' Project

---
+++ china/vocab.php
@@ -0,0 +1,113 @@
+<?php
+$pageTitle = "中文词汇";
+$page = "Strategy";
+include ("../inc/chinaHeader.php");
+?>
+<!-- ************************************************** -->
+<!-- ******************* <<STRATEGY>> ***************** -->
+
+<!-- // CONTENT-BEGINS ////////////////////////// -->
+<article>
+  <!-- // Vocabulary -->
+  <section>
+    <h1 class="title">Vocabulary</h1>
+    <!--<figure>
+      <img class="mainImage" src="img/puzzle2.jpg"/>
+      <figcaption>There will be Challenges</figcaption>
+    </figure>
+    <p>
+      Writing compelling copy and using media that properly articulates your message is of utmost importance before structuring a layout design for a website. Producing content that provides a meaningful user experience is the goal when executing a strategy. Topics covered:
+    </p>
+    <ol>
+      <li>Brainstorm</li>
+      <li>Content Basics</li>
+      <li>Information Architecture</li>
+    </ol> -->
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    
+      <aside class="vocabGrid">
+        <div class="word">
+          <div class="pinyin">&#224;i</div>
+          <div class="hanzi">爱</div>
+          <div class="translation">Love</div>
+        </div>
+        <div class="word">
+          <div class="pinyin">b&#227;ngzh&#249;</div>
+          <div class="hanzi">帮助</div>
+          <div class="translation">Help</div>
+        </div>
+        <div class="word">
+          <div class="pinyin">b&#227;n</div>
+          <div class="hanzi">搬</div>
+          <div class="translation">Move</div>
+        </div>
+        <div class="word">
+          <div class="pinyin">bi&#224;nhu&#224;</div>
+          <div class="hanzi">变化</div>
+          <div class="translation">Change</div>
+        </div>
+        <div class="word">
+          <div class="pinyin">biaoshi</div>
+          <div class="hanzi">表示</div>
+          <div class="translation">Indicate</div>
+        </div>
+        <div class="word">
+          <div class="pinyin">biaoyan</div>
+          <div class="hanzi">表演</div>
+          <div class="translation">Perform</div>
+        </div>
+        <div class="word">
+          <div class="pinyin">chi</div>
+          <div class="hanzi">吃</div>
+          <div class="translation">Eat</div>
+        </div>
+        <div class="word">
+          <div class="pinyin">changge</div>
+          <div class="hanzi">唱歌</div>
+          <div class="translation">Sing</div>
+        </div>
+        <div class="word">
+          <div class="pinyin">chu</div>
+          <div class="hanzi">出</div>
+          <div class="translation">Go Out</div>
+        </div>
+        <div class="word">
+          <div class="pinyin">chuan</div>
+          <div class="hanzi">穿</div>
+          <div class="translation">Wear</div>
+        </div>
+        <div class="word">
+          <div class="pinyin">canjia</div>
+          <div class="hanzi">参加</div>
+          <div class="translation">Join</div>
+        </div>
+        <div class="word">
+          <div class="pinyin">cha</div>
+          <div class="hanzi">差</div>
+          <div class="translation">Less (than)</div>
+        </div>
+        <div class="word">
+          <div class="pinyin">chidao</div>
+          <div class="hanzi">迟到</div>
+          <div class="translation">Late</div>
+        </div>
+        <div class="word">
+          <div class="pinyin">chuxian</div>
+          <div class="hanzi">出现</div>
+          <div class="translation">Appear</div>
+        </div>
+      </aside>
+  </section>
+  <!-- ************************************************** -->
+  <!-- ************************************************** -->
+</article>
+<!-- .....CONTENT-ENDS ////////////////////////// --> 
+
+
+<!-- ************************************************** -->
+<!-- ************************************************** -->
+<?php
+include ("../inc/footer.php"); 
+?>
