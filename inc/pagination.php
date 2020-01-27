cbfc1a296ff06b2475d9a82f473badf02d8d8175
MisterMoody <principal@mistermoody.com>
2020-01-27 14:02:37 -0500

    Add title='...' to Page Links

--- inc/pagination.php
+++ inc/pagination.php
@@ -1,11 +1,11 @@
 <section class="caseCenter">
   <ul class="pagination">
-    <li class="<?php echo ($page == "Workstation" ? "active" : "")?>"><a href="webdesign/workstation">1</a></li>
-    <li class="<?php echo ($page == "Strategy" ? "active" : "")?>"><a href="webdesign/strategy">2</a></li>
-    <li class="<?php echo ($page == "Semantic Markup" ? "active" : "")?>"><a href="webdesign/semantic-markup">3</a></li>
-    <li class="<?php echo ($page == "Styling Markup" ? "active" : "")?>"><a href="webdesign/styling-markup-with-css">4</a></li>
-    <li class="<?php echo ($page == "Optimization" ? "active" : "")?>"><a href="webdesign/optimization">5</a></li>
-    <li class="<?php echo ($page == "Git Control" ? "active" : "")?>"><a href="webdesign/git-control">6</a></li>
-    <li class="<?php echo ($page == "Resources" ? "active" : "")?>"><a href="webdesign/resources">7</a></li>
+    <li class="<?php echo ($page == "Workstation" ? "active" : "")?>"><a href="webdesign/workstation" title="Workstation">1</a></li>
+    <li class="<?php echo ($page == "Strategy" ? "active" : "")?>"><a href="webdesign/strategy" title="Strategy">2</a></li>
+    <li class="<?php echo ($page == "Semantic Markup" ? "active" : "")?>"><a href="webdesign/semantic-markup" title="Semantic Markup">3</a></li>
+    <li class="<?php echo ($page == "Styling Markup" ? "active" : "")?>"><a href="webdesign/styling-markup-with-css" title="Styling Markup">4</a></li>
+    <li class="<?php echo ($page == "Optimization" ? "active" : "")?>"><a href="webdesign/optimization" title="Optimization">5</a></li>
+    <li class="<?php echo ($page == "Git Control" ? "active" : "")?>"><a href="webdesign/git-control" title="Git Control">6</a></li>
+    <li class="<?php echo ($page == "Resources" ? "active" : "")?>"><a href="webdesign/resources" title="Resources">7</a></li>
   </ul>
 </section>
