4aeb74849a7fc27f371e47c267be65e26fff3842
MisterMoody <principal@mistermoody.com>
2020-02-10 10:59:52 -0500

    Refactor About Page Content + Rm WebDev Interest

--- about/index.php
+++ about/index.php
@@ -12,32 +12,26 @@ include ("../inc/header.php")
 </section>
   <section class="hideDesktop">
     <article>
-      <!-- Social Proof: @ School -->
-      <aside>
-        <p>
-          I am currently a substitute teacher with <a href="http://jefferson.kyschools.us" target="_blank">J.C.P.S.</a> KY serving the needs of youth kindergarten through grade 12. Employed since <code>January 2018</code>, I have been awarded <code>100+</code> assignments at <code>42</code> schools where I have facilitated lessons to more than <code>2,700</code> students.
-        </p>
-      </aside>
+      <!-- TEACHING Exp. + Social Proof: @ School -->
+      <p>
+        I am a substitute teacher with  <a href="http://jefferson.kyschools.us" target="_blank">J.C.P.S.</a> where I provide learning services to youth of all ages. I have held this position since <code>January 2018</code> and have been awarded <code>100+</code> assignments at <code>42</code> schools where I have facilitated lessons to more than <code>2,700</code> students.
+      </p>
+      <h2 style="text-align: center;">I Make Learning Fun!</h2>
       <aside class="imageGrid">
-        <img src="img/westernMS.jpg" style="height:12.8em;">
-        <img src="img/cleSTEMLab.png" style="height: 12.8em;">
-        <img src="img/GH-Shyrock.jpg" style="height: 12.8em;">
+        <img src="img/westernMS.jpg" style="height: 100%;">
+        <img src="img/guttermuthES.jpg">
+        <img src="img/GH-Shyrock.jpg" style="height: 100%;">
         
         <img src="img/coleridgeES.jpg" style="height: 100%;">
         <img src="img/brandeisES.jpg">
       </aside>
       <!-- Career Experience --> 
-      <aside>
-        <p>
-          My career as an educator began while studying at the University of Louisville where I earned a degree in political science. It was here I gained leadership experience through a plethora of opportunities, but it was my role serving at-risk youth for a <a href="https://www.cabbagepatch.org/" target="_blank">non-profit</a> that convinced me of my calling: I was 19. 
-        </p>
-        <p>
-          Shortly after graduation, I embarked on a sojourn abroad to teach English as a second language. What was supposed to have been a gap year before enrolling in law school turned into a life altering foreign cultural immersive experience.
-        </p>
-        <p>
-          Life as an emigrant was challenging, yet, as a life-learner having formally studied Francais, Espanol, Hangul, Nihongo and Putonghua, I empathize well with language acquisition struggles learners encounter. Some of the agencies that enabled me to practice my craft include Sogang University, Oxford Seminars, Joongdong High School, YBM Academy, Chien-Shiung Institute of Technology, Tongxiang Middle School, Shanghai Ocean University and Jiaxing College.
-        </p>
-      </aside>
+      <p>
+        My career as an educator began while studying at the University of Louisville where I earned a degree in political science. It was here I gained leadership experience through a plethora of opportunities, but it was my role serving at-risk youth for a <a href="https://www.cabbagepatch.org/" target="_blank">non-profit</a> that convinced me of my calling: I was 19. 
+      </p>
+      <p>
+        Shortly after graduation, I embarked on a sojourn abroad to teach English as a second language. What was supposed to have been a gap year before enrolling in law school turned into a life altering foreign cultural immersive experience.
+      </p>
       <!-- Social Proof: @ Asia -->
       <aside class="imageGrid">
         <img src="img/oceanuniv.jpg">
@@ -52,9 +46,16 @@ include ("../inc/header.php")
         <img src="img/jdhs.jpg">
       </aside>
       <p>
+        Life as an emigrant was challenging, yet, as a life-learner having formally studied Francais, Espanol, Hangul, Nihongo and Putonghua, I empathize well with language acquisition struggles learners encounter. Some of the agencies that enabled me to practice my craft include Sogang University, Oxford Seminars, Joongdong High School, YBM Academy, Chien-Shiung Institute of Technology, Tongxiang Middle School, Shanghai Ocean University and Jiaxing College.
+      </p>
+      <!-- WebDev INTEREST -->
+      <!--<p>
         My interest in web technologies stems from my participation in <a href="https://codelouisville.org" target="_blank">&#60;code&#62; Louisville</a>, a web dev platform that introduces best practices and concepts related to front-end web development. To this end, I can design, develop and deploy a website to specification. Currently doing my part to build the ecosystem, most recently helping organize a local <a href="https://startupweekendlouisville.com/" target="_blank">startup event</a>. Working on an <a href="webdesign/">e-book</a> for folks interested in learning the craft.
       </p>
-      <figure><img class="mainImage" src="img/startupWKND.jpg"></figure>
+      <figure>
+        <img class="mainImage" src="img/startupWKND.jpg">
+      <figcaption>JCPS students attend Startup Weekend</figcaption>
+      </figure>-->
       <!-- Call-to-Action BTN -->
       <aside>
         <button class="cta"><a href="portfolio/" alt="link to portfolio">Check out my Portfolio</a></button> 
