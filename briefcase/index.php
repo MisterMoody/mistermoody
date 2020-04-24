45fcfbd1567a798b61c94d32e6bbf9517886091e
MisterMoody <principal@mistermoody.com>
2020-04-24 13:02:24 -0400

    General Edits

--- briefcase/index.php
+++ briefcase/index.php
@@ -21,199 +21,48 @@ include ("../inc/header.php");
     
     <section class="chapter__book">
       <aside class="chapter">
-        <h3 class="chapter__Headline">Excel Visualizations</h3>
-        <p>
-          Basic examples of data visualizations using Excel. <a href="data/excel-visualizations">Read</a>
-        </p>
-        <div class="chapter__Date_Tag">
-          <div>
-            <i class="fa fa-clock-o" aria-hidden="true"></i> March 2020
-          </div>
-          <div class="chapter__Tags">
-            <div>Data Analysis</div>
-          </div>
-        </div>
-      </aside>
-      <aside class="chapter">
-        <h3 class="chapter__Headline">Excel Formula Concepts</h3>
-        <p>
-          A general overview of how to collect, analyze and visualize data using Excel. <a href="data/excel-formula-concepts">Read</a>
-        </p>
-        <div class="chapter__Date_Tag">
-          <div>
-            <i class="fa fa-clock-o" aria-hidden="true"></i> March 2020
-          </div>
-          <div class="chapter__Tags">
-            <div>Data Analysis</div>
-          </div>
-        </div>
-      </aside>
-      <aside class="chapter">
         <h3 class="chapter__Headline">The Data Analysis Process</h3>
         <p>
-          An brief on how I collect, analyze and visualize data using Microsoft Excel. <a href="data/the-data-analysis-process">Read</a>
+          A brief on how I collect, analyze and visualize data using Microsoft Excel. <a href="data/the-data-analysis-process">Read</a>
         </p>
         <div class="chapter__Date_Tag">
           <div>
             <i class="fa fa-clock-o" aria-hidden="true"></i> March 2020
           </div>
           <div class="chapter__Tags">
-            <div>Data Analysis</div>
+            <div><a href="data/">Data Analysis</a></div>
+          </div>
+        </div>
+      </aside>
+      <aside class="chapter">
+        <h3 class="chapter__Headline">中文学习</h3>
+        <p>
+          List of vocabulary required to pass the Hanyu Shuiping Kaoshi. <a href="china/vocab">Read</a>
+        </p>
+        <div class="chapter__Date_Tag">
+          <div>
+            <i class="fa fa-clock-o" aria-hidden="true"></i> February 2020
+          </div>
+          <div class="chapter__Tags">
+            <div>中文</div>
           </div>
         </div>
       </aside>
       <aside class="chapter">
         <h3 class="chapter__Headline">Website Toolkit</h3>
         <p>
-          Articles, examples and resources for building fully functional websites. <a href="webdev/toolkit/">Read</a>
+          Articles, examples and resources for building functional websites. <a href="webdev/toolkit/">Read</a>
         </p>
         <div class="chapter__Date_Tag">
           <div>
             <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
           </div>
           <div class="chapter__Tags">
-            <div>Web Development</div>
+            <div><a href="webdev/">Web Development</a></div>
           </div>
         </div>
       </aside>
-      <!--<aside class="chapter">
-        <h3 class="chapter__Headline">Workstation</h3>
-        <p>
-            Building a website requires utilizing technologies that facilitates the process of bringing an idea to life in the form of a website. This can be achieved with a text-editor, a version controller and two servers. &nbsp;
-            <a href="webdev/workstation">Read</a>
-          </p>
-        <div class="chapter__Date_Tag">
-          <div>
-            <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
-          </div>
-          <div class="chapter__Tags">
-            <div>IDE</div>
-            <div>Server</div>
-            <div>VCS</div>
-             <div>DevOps</div>
-          </div>
-        </div>
-      </aside>
-      
-      <aside class="chapter">
-        <h3 class="chapter__Headline">Content Strategy</h3>
-        <p>
-          Compelling copy and intriguing media are mediums used to articular a message. Before structuring the layout design for a website, it is imperative to create content that provides a meaningful user experience. &nbsp;
-            <a href="webdev/strategy">Read</a>
-          </p>
-        <div class="chapter__Date_Tag">
-          <div>
-            <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
-          </div>
-          <div class="chapter__Tags">
-            <div>Design</div>
-            <div>Marketing</div>
-            <div>Research</div>
-          </div>
-        </div>
-      </aside>
-      
-      <aside class="chapter">
-        <h3 class="chapter__Headline">Semantic Markup</h3>
-        <p>
-          The Hyper Text Markup Language (HTML) is used to markup content: copy and media to be displayed in a browser for user viewing. All content is composed of elements, which have a default functionality that can be modified with attributes. &nbsp;
-            <a href="webdev/semantic-markup">Read</a>
-          </p>
-        <div class="chapter__Date_Tag">
-          <div>
-            <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
-          </div>
-           <div class="chapter__Tags">
-             <div>HTML</div>
-             <div>Markup</div>
-           </div>
-         </div>
-      </aside>
-      
-      <aside class="chapter">
-        <h3 class="chapter__Headline">Styling Markup</h3>
-        <p>
-          Cascading Style Sheets are used to define semantic markup. CSS selectors are used to target HTML elements which invoke declarations of property-value pairings bringing about the desired aesthetic quality. There are hundreds of properties available for to provide an expression of a message!  &nbsp; 
-            <a href="webdev/styling-markup-with-css/">Read</a>
-          </p>
-        <div class="chapter__Date_Tag">
-          <div>
-            <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
-          </div>
-          <div class="chapter__Tags">
-            <div>CSS</div>
-            <div>Design</div>
-          </div>
-        </div>
-      </aside>  
-      
-      <aside class="chapter">
-        <h3 class="chapter__Headline">Optimization</h3>
-        <p>
-          Building a pretty website is cool, but its true quality is valued in how well it performs. There are tools that can be used to enhance user experience by delivering assets to users expeditiously. &nbsp;
-          <a href="webdev/resources">Read</a>
-        </p>
-        <div class="chapter__Date_Tag">
-          <div>
-            <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
-          </div>
-         </div>
-      </aside>  
-      
-      <aside class="chapter">
-        <h3 class="chapter__Headline">Version Control</h3>
-        <p>
-          Version control systems enable developers to collaborate, save and ship code from a repository that can be accessed anywhere in the world. &nbsp;
-          <a href="webdev/resources">Read</a>
-        </p>
-        <div class="chapter__Date_Tag">
-          <div>
-            <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
-          </div>
-         </div>
-      </aside>  
-      
-      <aside class="chapter">
-        <h3 class="chapter__Headline">Resources</h3>
-        <p>
-          Boilerplates. Placeholders. Performance Testers. Formatters. Forms. Font-Scale. Typography. Color Swatches + Palettes. Contrast Checker. Infographics. Image Optimizer. Icons. Accessibility (A11Y). Compressors. Validators. Tutorials. UX Inspiration. ASCII. &nbsp;
-          <a href="webdev/resources">Read</a>
-        </p>
-        <div class="chapter__Date_Tag">
-          <div>
-            <i class="fa fa-clock-o" aria-hidden="true"></i> January 2020
-          </div>
-           <div class="chapter__Tags">
-             <div>Front-end</div>
-             <div>Back-end</div>
-             <div>DevOps</div>
-           </div>
-         </div>
-      </aside>-->
     </section>
-  </article>
-
-  <article class="cur__TopicGrid">
-    <section class="cur__TopicStyle">
-      <div><h3>中文</h3></div>
-      <p><a href="china/vocab">N / A</a></p>
-    </section>
-    <section class="cur__TopicStyle">
-      <div><h3>Data Analysis</h3></div>
-      <p><a href="data/">Enter</a></p>
-    </section>
-    <section class="cur__TopicStyle">
-      <div><h3>Web Development</h3></div>
-      <p><a href="webdev/">Enter</a></p>
-    </section>
-    <section class="cur__TopicStyle">
-      <div><h3>Language Arts</h3></div>
-      <p><a href="briefcase/">N / A</a></p>
-    </section>
-    <!--<section class="cur__TopicStyle">
-      <div><h3>Politics</h3></div>
-      <p><a href="briefcase/">N / A</a></p>
-    </section>-->
   </article>
 <!-- ******************************************************* -->
 <!-- ******************************************************* -->
